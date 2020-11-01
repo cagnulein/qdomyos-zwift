bde4c5f5ccbb139facfe955427e539fd6333b220
Roberto Viola <cagnulein@gmail.com>
2020-11-01 19:02:42 +0100

    trying to fix cadence instability
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/domyosbike.cpp
+++ src/domyosbike.cpp
@@ -178,8 +178,8 @@ void domyosbike::characteristicChanged(const QLowEnergyCharacteristic &character
     Resistance = newValue.at(14);
     Heart = newValue.at(18);
 
-    CrankRevs += ((double)(lastRefresh.msecsTo(QTime::currentTime())) * ((double)Cadence / 60000.0) );
-    LastCrankEventTime += (uint16_t)((lastRefresh.msecsTo(QTime::currentTime())) * 0.9765625);
+    CrankRevs += (double)Cadence; //((double)(lastRefresh.msecsTo(QTime::currentTime())) * ((double)Cadence / 60000.0) );
+    LastCrankEventTime += 1024; //(uint16_t)((lastRefresh.msecsTo(QTime::currentTime())) * 0.9765625);
     lastRefresh = QTime::currentTime();
 
     debug("Current speed: " + QString::number(speed));
