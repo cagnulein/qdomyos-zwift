09defca49be3cf49a159fde1547230ad77cf0745
Roberto Viola <cagnulein@gmail.com>
2020-11-05 08:51:37 +0100

    fixed odometer to the console of the domyosbike
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/domyosbike.cpp
+++ src/domyosbike.cpp
@@ -63,8 +63,8 @@ void domyosbike::updateDisplay(uint16_t elapsed)
                          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
                          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
 
-   display[19] = ((((uint16_t)odometer())) >> 8) & 0xFF;
-   display[20] = (((uint16_t)odometer())) & 0xFF;
+   display[3] = ((((uint16_t)odometer())) >> 8) & 0xFF;
+   display[4] = (((uint16_t)odometer())) & 0xFF;
 
    for(uint8_t i=0; i<sizeof(display2)-1; i++)
    {
