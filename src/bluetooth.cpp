3b489fda3fcec1f8275d994c5c37acfd8a14b603
Roberto Viola <cagnulein@gmail.com>
2020-12-09 15:16:30 +0100

    crash fixed when a bluetooth device disconnects
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -128,9 +128,17 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device)
 
 void bluetooth::restart()
 {
-    if(domyos)
+    if(device()->VirtualDevice())
     {
-        delete domyos;
+        if(device()->deviceType() == bluetoothdevice::TREADMILL)
+            delete (virtualtreadmill*)device()->VirtualDevice();
+        else if(device()->deviceType() == bluetoothdevice::BIKE)
+            delete (virtualbike*)device()->VirtualDevice();
+    }
+
+    if(domyos)
+    {        
+        delete domyos;        
         domyos = 0;
     }
     if(domyosBike)
