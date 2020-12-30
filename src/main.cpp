b668f0e1e5f2b0658ade8fe84c911d0753bb0bdc
Roberto Viola <cagnulein@gmail.com>
2020-12-30 19:28:06 +0100

    version 1.9.0
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/main.cpp
+++ src/main.cpp
@@ -250,7 +250,7 @@ int main(int argc, char *argv[])
 #endif
 
     qInstallMessageHandler(myMessageOutput);
-    qDebug() << "version 1.8.9";
+    qDebug() << "version 1.9.0";
 
 #if 0 // test gpx or fit export
     QList<SessionLine> l;
