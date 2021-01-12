c03996560df105f5d318e1148bd3dff9acdc9316
Roberto Viola <cagnulein@gmail.com>
2021-01-12 16:15:46 +0100

    fixed watt overflow on yesoulbike
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/yesoulbike.cpp
+++ src/yesoulbike.cpp
@@ -138,7 +138,7 @@ void yesoulbike::characteristicChanged(const QLowEnergyCharacteristic &character
 
     Resistance = newValue.at(4);
     Cadence = newValue.at(6);
-    m_watts = ((newValue.at(7) << 8) | newValue.at(8));
+    m_watts = ((((uint16_t)newValue.at(7)) << 8) + ((uint16_t) newValue.at(8)));
     Speed = 0.37497622 * ((double)Cadence.value());
     KCal += ((( (0.048 * ((double)watts()) + 1.19) * settings.value("weight", 75.0).toFloat() * 3.5) / 200.0 ) / (60000.0 / ((double)lastRefreshCharacteristicChanged.msecsTo(QDateTime::currentDateTime())))); //(( (0.048* Output in watts +1.19) * body weight in kg * 3.5) / 200 ) / 60
     Distance += ((Speed.value() / 3600000.0) * ((double)lastRefreshCharacteristicChanged.msecsTo(QDateTime::currentDateTime())) );
