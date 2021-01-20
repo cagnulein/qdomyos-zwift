b2c63566850d02a1faea5a9cd5ea7258e963f904
Roberto Viola <cagnulein@gmail.com>
2021-01-20 09:31:55 +0100

    version 2.2.5
    Signed-off-by: Roberto Viola <roberto.viola@systemceramics.com>

--- src/main.cpp
+++ src/main.cpp
@@ -254,7 +254,7 @@ int main(int argc, char *argv[])
 #endif
 
     qInstallMessageHandler(myMessageOutput);
-    qDebug() << "version 2.2.4";
+    qDebug() << "version 2.2.5";
 
 #if 0 // test gpx or fit export
     QList<SessionLine> l;
