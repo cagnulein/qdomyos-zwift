f5765abdcab0c4a86ea22408a33bbce322cb6c61
Roberto Viola <roberto.viola@systemceramics.com>
2021-02-10 08:34:28 +0100

    add read char and descriptorRead on ftmsbike

--- src/ftmsbike.cpp
+++ src/ftmsbike.cpp
@@ -333,17 +333,21 @@ void ftmsbike::stateChanged(QLowEnergyService::ServiceState state)
 
     foreach(QLowEnergyService* s, gattCommunicationChannelService)
     {
-        if(s->state() != QLowEnergyService::InvalidService)
+        if(s->state() == QLowEnergyService::ServiceDiscovered)
         {
             // establish hook into notifications
             connect(s, SIGNAL(characteristicChanged(QLowEnergyCharacteristic,QByteArray)),
                     this, SLOT(characteristicChanged(QLowEnergyCharacteristic,QByteArray)));
             connect(s, SIGNAL(characteristicWritten(const QLowEnergyCharacteristic, const QByteArray)),
                     this, SLOT(characteristicWritten(const QLowEnergyCharacteristic, const QByteArray)));
+            connect(s, SIGNAL(characteristicRead(const QLowEnergyCharacteristic, const QByteArray)),
+                    this, SLOT(characteristicRead(const QLowEnergyCharacteristic, const QByteArray)));
             connect(s, SIGNAL(error(QLowEnergyService::ServiceError)),
                     this, SLOT(errorService(QLowEnergyService::ServiceError)));
             connect(s, SIGNAL(descriptorWritten(const QLowEnergyDescriptor, const QByteArray)), this,
                     SLOT(descriptorWritten(const QLowEnergyDescriptor, const QByteArray)));
+            connect(s, SIGNAL(descriptorRead(const QLowEnergyDescriptor, const QByteArray)), this,
+                    SLOT(descriptorRead(const QLowEnergyDescriptor, const QByteArray)));
 
             qDebug() << s->serviceUuid() << "connected!";
 
@@ -429,10 +433,20 @@ void ftmsbike::descriptorWritten(const QLowEnergyDescriptor &descriptor, const Q
     emit connectedAndDiscovered();
 }
 
+void ftmsbike::descriptorRead(const QLowEnergyDescriptor &descriptor, const QByteArray &newValue)
+{
+    qDebug() << "descriptorRead " << descriptor.name() << descriptor.uuid() << newValue.toHex(' ');
+}
+
 void ftmsbike::characteristicWritten(const QLowEnergyCharacteristic &characteristic, const QByteArray &newValue)
 {
     Q_UNUSED(characteristic);
     debug("characteristicWritten " + newValue.toHex(' '));
+}
+
+void ftmsbike::characteristicRead(const QLowEnergyCharacteristic &characteristic, const QByteArray &newValue)
+{
+    qDebug() << "characteristicRead " << characteristic.uuid() << newValue.toHex(' ');
 }
 
 void ftmsbike::serviceScanDone(void)
