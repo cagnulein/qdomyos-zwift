d3cebaa79e553865bd107190a91a909786c206a2
Roberto Viola <roberto.viola@systemceramics.com>
2021-04-02 13:22:12 +0200

    S77 treadmill compatibility added

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -245,7 +245,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device)
                 if(!discoveryAgent->isActive())
                     emit searchingStop();
             }
-            else if((b.name().toUpper().startsWith("HORIZON") || b.name().toUpper().startsWith("F80") || b.name().toUpper().startsWith("ESANGLINKER")) && !horizonTreadmill && filter)
+            else if((b.name().toUpper().startsWith("HORIZON") || b.name().toUpper().startsWith("F80") || b.name().toUpper().startsWith("S77") || b.name().toUpper().startsWith("ESANGLINKER")) && !horizonTreadmill && filter)
             {
                 discoveryAgent->stop();
                 horizonTreadmill = new horizontreadmill(noWriteResistance, noHeartService);
