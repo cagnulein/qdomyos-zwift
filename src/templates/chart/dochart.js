24eb35c60ef21849749d10e96ac7d43a1bed7d8d
Roberto Viola <roberto.viola@systemceramics.com>
2021-07-15 11:36:43 +0200

    heart rate chart and fixed resolution for mobile

--- src/templates/chart/dochart.js
+++ src/templates/chart/dochart.js
@@ -11,23 +11,31 @@ window.chartColors = {
     white: 'rgb(255, 255, 255)',
     limegreen: 'rgb(50, 205, 50)',
     gold: 'rgb(255, 215, 0)',
+    black: 'rgb(0, 0, 0)',
 };
 
 var ftp = 200;
 var ftpZones = [];
+var maxHeartRate = 190;
+var heartZones = [];
 
 function process_arr(arr) {
     let watts = [];
     let reqpower = [];
+    let heart = [];
     for (let el of arr) {
         let wattel = {};
         let reqpowerel = {};
+        let heartel = {};
         wattel.x = el.elapsed_s + el.elapsed_m * 60 + el.elapsed_h * 3600;
         wattel.y = el.watts;
         watts.push(wattel);
         reqpowerel.x = el.elapsed_s + el.elapsed_m * 60 + el.elapsed_h * 3600;
         reqpowerel.y = el.req_power;
         reqpower.push(reqpowerel);
+        heartel.x = el.elapsed_s + el.elapsed_m * 60 + el.elapsed_h * 3600;
+        heartel.y = el.heart;
+        heart.push(heartel);
     }
     let config = {
         type: 'line',
@@ -39,6 +47,7 @@ function process_arr(arr) {
                 cubicInterpolationMode: 'monotone',
                 data: watts,
                 fill: false,
+                pointRadius: 0,
                 segment: {
                    borderColor: ctx => ctx.p0.parsed.y < ftpZones[0] ? window.chartColors.white :
                                                                        ctx.p0.parsed.y < ftpZones[1] ? window.chartColors.limegreen :
@@ -50,17 +59,19 @@ function process_arr(arr) {
                 }
             }, {
                 label: 'Req. Watts',
-                backgroundColor: window.chartColors.red,
+                backgroundColor: window.chartColors.black,
                 borderColor: window.chartColors.black,
                 borderWidth: 5,
                 cubicInterpolationMode: 'monotone',
                 data: reqpower,
                 fill: false,
+                pointRadius: 0,
             },
             ]
         },
         options: {
             responsive: true,
+            aspectRatio: 1.5,
             grid: {
                 zeroLineColor: 'rgba(0,255,0,1)'
             },
@@ -103,11 +114,11 @@ function process_arr(arr) {
                         stepSize: 1,
                         autoSkip: false,
                         callback: value => [ftpZones[0], ftpZones[1], ftpZones[2], ftpZones[3], ftpZones[4], ftpZones[5]].includes(value) ?
-                            value === ftpZones[0] ? 'zone 1' :
-                            value === ftpZones[1] ? 'zone 2' :
-                            value === ftpZones[2] ? 'zone 3' :
-                            value === ftpZones[3] ? 'zone 4' :
-                            value === ftpZones[4] ? 'zone 5' : undefined : undefined,
+                            value === ftpZones[0] ? 'zone 2' :
+                            value === ftpZones[1] ? 'zone 3' :
+                            value === ftpZones[2] ? 'zone 4' :
+                            value === ftpZones[3] ? 'zone 5' :
+                            value === ftpZones[4] ? 'zone 6' : undefined : undefined,
                         color: 'black',
                     }
                 }
@@ -117,11 +128,90 @@ function process_arr(arr) {
 
     let ctx = document.getElementById('canvas').getContext('2d');
     new Chart(ctx, config);
+
+    config = {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Heart',
+                backgroundColor: window.chartColors.red,
+                borderColor: window.chartColors.red,
+                cubicInterpolationMode: 'monotone',
+                data: heart,
+                fill: false,
+                pointRadius: 0,
+                segment: {
+                   borderColor: ctx => ctx.p0.parsed.y < heartZones[0] ? window.chartColors.lightsteelblue :
+                                                                       ctx.p0.parsed.y < heartZones[1] ? window.chartColors.green :
+                                                                       ctx.p0.parsed.y < heartZones[2] ? window.chartColors.yellow :
+                                                                       ctx.p0.parsed.y < heartZones[3] ? window.chartColors.orange :
+                                                                       window.chartColors.red,
+                }
+            }]
+        },
+        options: {
+            responsive: true,
+            aspectRatio: 1.5,
+            grid: {
+                zeroLineColor: 'rgba(0,255,0,1)'
+            },
+            plugins: {
+                title:{
+                    display:true,
+                    text:'Heart Rate'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                }
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                x: {
+                    type: 'linear',
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Time'
+                    },
+                    ticks: {
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return Math.floor(value / 3600).toString().padStart(2, "0") + ":" + Math.floor(value / 60).toString().padStart(2, "0");
+                        }
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Heart rate'
+                    },
+                    ticks: {
+                        stepSize: 1,
+                        autoSkip: false,
+                        callback: value => [heartZones[0], heartZones[1], heartZones[2], heartZones[3], heartZones[4]].includes(value) ?
+                            value === heartZones[0] ? 'zone 2' :
+                            value === heartZones[1] ? 'zone 3' :
+                            value === heartZones[2] ? 'zone 4' :
+                            value === heartZones[3] ? 'zone 5' : undefined : undefined,
+                        color: 'black',
+                    }
+                }
+            }
+        }
+    };
+
+    ctx = document.getElementById('canvasHeart').getContext('2d');
+    new Chart(ctx, config);
 }
 
 function dochart_init() {
     onSettingsOK = true;
-    keys_arr = ['ftp', 'heart_rate_zone1', 'heart_rate_zone2', 'heart_rate_zone3', 'heart_rate_zone4']
+    keys_arr = ['ftp', 'age', 'heart_rate_zone1', 'heart_rate_zone2', 'heart_rate_zone3', 'heart_rate_zone4']
     let el = new MainWSQueueElement({
             msg: 'getsettings',
             content: {
@@ -140,6 +230,17 @@ function dochart_init() {
                         ftpZones[3] = Math.round(ftp * 1.05);
                         ftpZones[4] = Math.round(ftp * 1.20);
                         ftpZones[5] = Math.round(ftp * 1.50);
+                    } else if (key === 'age') {
+                        age = msg.content[key];
+                        maxHeartRate = 220 - age;
+                    } else if (key === 'heart_rate_zone1') {
+                        heartZones[0] = Math.round(maxHeartRate * msg.content[key]);
+                    } else if (key === 'heart_rate_zone2') {
+                        heartZones[1] = Math.round(maxHeartRate * msg.content[key]);
+                    } else if (key === 'heart_rate_zone3') {
+                        heartZones[2] = Math.round(maxHeartRate * msg.content[key]);
+                    } else if (key === 'heart_rate_zone4') {
+                        heartZones[3] = Math.round(maxHeartRate * msg.content[key]);
                     }
                 }
                 return msg.content;
@@ -165,4 +266,40 @@ function dochart_init() {
 
 $(window).on('load', function () {
     dochart_init();
+
+    // DEBUG
+    ftpZones[0] = Math.round(ftp * 0.55);
+    ftpZones[1] = Math.round(ftp * 0.75);
+    ftpZones[2] = Math.round(ftp * 0.90);
+    ftpZones[3] = Math.round(ftp * 1.05);
+    ftpZones[4] = Math.round(ftp * 1.20);
+    ftpZones[5] = Math.round(ftp * 1.50);
+
+    heartZones[0] = 110;
+    heartZones[1] = 130;
+    heartZones[2] = 150;
+    heartZones[3] = 170;
+
+    arr = [{'watts': 100, 'req_power': 150, 'elapsed_s':0,'elapsed_m':0,'elapsed_h':0, 'heart':90},
+           {'watts': 120, 'req_power': 150, 'elapsed_s':1,'elapsed_m':1,'elapsed_h':0, 'heart':92},
+           {'watts': 130, 'req_power': 170, 'elapsed_s':2,'elapsed_m':2,'elapsed_h':0, 'heart':110},
+           {'watts': 140, 'req_power': 170, 'elapsed_s':3,'elapsed_m':3,'elapsed_h':0, 'heart':115},
+           {'watts': 130, 'req_power': 170, 'elapsed_s':4,'elapsed_m':4,'elapsed_h':0, 'heart':130},
+           {'watts': 160, 'req_power': 170, 'elapsed_s':5,'elapsed_m':5,'elapsed_h':0, 'heart':135},
+           {'watts': 180, 'req_power': 130, 'elapsed_s':6,'elapsed_m':6,'elapsed_h':0, 'heart':140},
+           {'watts': 120, 'req_power': 130, 'elapsed_s':7,'elapsed_m':7,'elapsed_h':0, 'heart':150},
+           {'watts': 190, 'req_power': 150, 'elapsed_s':1,'elapsed_m':8,'elapsed_h':0, 'heart':155},
+           {'watts': 195, 'req_power': 170, 'elapsed_s':2,'elapsed_m':9,'elapsed_h':0, 'heart':165},
+           {'watts': 200, 'req_power': 170, 'elapsed_s':3,'elapsed_m':10,'elapsed_h':0, 'heart':153},
+           {'watts': 206, 'req_power': 170, 'elapsed_s':4,'elapsed_m':11,'elapsed_h':0, 'heart':152},
+           {'watts': 211, 'req_power': 170, 'elapsed_s':5,'elapsed_m':12,'elapsed_h':0, 'heart':180},
+           {'watts': 222, 'req_power': 130, 'elapsed_s':6,'elapsed_m':13,'elapsed_h':0, 'heart':182},
+           {'watts': 237, 'req_power': 130, 'elapsed_s':7,'elapsed_m':14,'elapsed_h':0, 'heart':160},
+           {'watts': 250, 'req_power': 170, 'elapsed_s':3,'elapsed_m':15,'elapsed_h':0, 'heart':115},
+           {'watts': 266, 'req_power': 170, 'elapsed_s':4,'elapsed_m':16,'elapsed_h':0, 'heart':120},
+           {'watts': 271, 'req_power': 170, 'elapsed_s':5,'elapsed_m':17,'elapsed_h':0, 'heart':112},
+           {'watts': 262, 'req_power': 130, 'elapsed_s':6,'elapsed_m':18,'elapsed_h':0, 'heart':90},
+           {'watts': 257, 'req_power': 130, 'elapsed_s':7,'elapsed_m':19,'elapsed_h':0, 'heart':120},
+            ]
+    process_arr(arr);
 });
