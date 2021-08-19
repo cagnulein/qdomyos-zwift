587b342569ec4426bbf13ee81d982b401969e1e0
Roberto Viola <cagnulein@gmail.com>
2021-08-19 15:30:27 +0200

    added Bodytone SMB1 bike to version 2.8.37

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -468,7 +468,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                 innerTemplateManager->start(npeCableBike);
             } else if (((b.name().startsWith("FS-") && hammerRacerS) ||
                         (b.name().toUpper().startsWith("WAHOO KICKR")) || (b.name().toUpper().startsWith("B94")) ||
-                        (b.name().toUpper().startsWith("STAGES BIKE"))) &&
+                        (b.name().toUpper().startsWith("STAGES BIKE")) || (b.name().toUpper().startsWith("SMB1"))) &&
                        !ftmsBike && !snodeBike && !fitPlusBike && !stagesBike && filter) {
                 discoveryAgent->stop();
                 ftmsBike = new ftmsbike(noWriteResistance, noHeartService);
