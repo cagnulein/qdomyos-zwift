2bb5a588c620fd482a1c8b295cb203d83f34f7b2
Roberto Viola <cagnulein@gmail.com>
2021-10-09 12:27:38 +0200

    fix build issue

--- src/fakebike.cpp
+++ src/fakebike.cpp
@@ -91,6 +91,7 @@ void fakebike::update() {
             if (ios_peloton_workaround && cadence && h && firstStateChanged) {
                 h->virtualbike_setCadence(currentCrankRevolutions(), lastCrankEventTime());
                 h->virtualbike_setHeartRate((uint8_t)metrics_override_heartrate());
+            }
     #endif
     #endif
     }
