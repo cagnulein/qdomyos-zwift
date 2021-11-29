5ee67336e9a116a4069b1fd6f5818de481b6601c
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2021-11-29 14:57:41 +0100

    Removed test code from heart rate characteristic notifier

--- src/characteristicnotifier2a37.cpp
+++ src/characteristicnotifier2a37.cpp
@@ -1,12 +1,10 @@
 #include "characteristicnotifier2a37.h"
-#include <stdlib.h>
 
 CharacteristicNotifier2A37::CharacteristicNotifier2A37(bluetoothdevice *Bike, QObject *parent)
     : CharacteristicNotifier(parent), Bike(Bike) {}
 
 int CharacteristicNotifier2A37::notify(QByteArray &valueHR) {
-    uint8_t hr = Bike->metrics_override_heartrate();
-    valueHR.append(char(0));                       // Flags that specify the format of the value.
-    valueHR.append(char(!hr ? rand() % 200 : hr)); // Actual value.
+    valueHR.append(char(0));                                  // Flags that specify the format of the value.
+    valueHR.append(char(Bike->metrics_override_heartrate())); // Actual value.
     return CN_OK;
 }
