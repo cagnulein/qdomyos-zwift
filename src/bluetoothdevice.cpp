8396a93c78170f519f9abc27892a1a25e470efd2
Roberto Viola <roberto.viola@systemceramics.com>
2021-12-01 15:58:31 +0100

    limiting fan speed to 100 #505

--- src/bluetoothdevice.cpp
+++ src/bluetoothdevice.cpp
@@ -98,6 +98,8 @@ bool bluetoothdevice::changeFanSpeed(uint8_t speed) {
         // managing overflow
         speed = 100;
     }
+    if (speed > 100)
+        speed = 100;
     // this is useful when there is a fitmetria fanfit in order to set the current
     // value to the last requested
     FanSpeed = speed;
