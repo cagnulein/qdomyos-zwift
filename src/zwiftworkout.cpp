82c6f6763110a6711106f7f2f502ab71b9234b70
Roberto Viola <roberto.viola@systemceramics.com>
2022-01-20 10:07:26 +0100

    ZWO workout file with treadmill not working #589

--- src/zwiftworkout.cpp
+++ src/zwiftworkout.cpp
@@ -9,6 +9,17 @@ QList<trainrow> zwiftworkout::load(const QString &filename) {
     return load(input.readAll());
 }
 
+bool zwiftworkout::durationAsDistance(QString sportType, QString durationType) {
+    // watch out to the compatibility with HomeFitnessBuddy
+    if (!sportType.toLower().contains(QStringLiteral("run")))
+        return false;
+    else if (sportType.toLower().contains(QStringLiteral("run")) &&
+             durationType.toLower().contains(QStringLiteral("time")))
+        return false;
+    else
+        return true;
+}
+
 QList<trainrow> zwiftworkout::load(const QByteArray &input) {
     QSettings settings;
     QList<trainrow> list;
@@ -55,10 +66,16 @@ QList<trainrow> zwiftworkout::load(const QByteArray &input) {
 
                 for (uint32_t i = 0; i < repeat; i++) {
                     trainrow row;
-                    row.duration = QTime(OnDuration / 3600, OnDuration / 60, OnDuration % 60, 0);
+                    if (!durationAsDistance(sportType, durationType))
+                        row.duration = QTime(OnDuration / 3600, OnDuration / 60, OnDuration % 60, 0);
+                    else
+                        row.distance = OnDuration / 1000.0;
                     row.power = OnPower * settings.value(QStringLiteral("ftp"), 200.0).toDouble();
                     list.append(row);
-                    row.duration = QTime(OffDuration / 3600, OffDuration / 60, OffDuration % 60, 0);
+                    if (!durationAsDistance(sportType, durationType))
+                        row.duration = QTime(OffDuration / 3600, OffDuration / 60, OffDuration % 60, 0);
+                    else
+                        row.distance = OffDuration / 1000.0;
                     row.power = OffPower * settings.value(QStringLiteral("ftp"), 200.0).toDouble();
                     list.append(row);
                 }
@@ -74,7 +91,10 @@ QList<trainrow> zwiftworkout::load(const QByteArray &input) {
                 }
 
                 trainrow row;
-                row.duration = QTime(Duration / 3600, Duration / 60, Duration % 60, 0);
+                if (!durationAsDistance(sportType, durationType))
+                    row.duration = QTime(Duration / 3600, Duration / 60, Duration % 60, 0);
+                else
+                    row.distance = Duration / 1000;
                 list.append(row);
             } else if (stream.name().contains(QStringLiteral("Ramp")) ||
                        stream.name().contains(QStringLiteral("Cooldown"))) {
@@ -93,7 +113,10 @@ QList<trainrow> zwiftworkout::load(const QByteArray &input) {
 
                 for (uint32_t i = 0; i < Duration; i++) {
                     trainrow row;
-                    row.duration = QTime(0, 0, 1, 0);
+                    if (!durationAsDistance(sportType, durationType))
+                        row.duration = QTime(0, 0, 1, 0);
+                    else
+                        row.distance = 0.001;
                     if (PowerHigh > PowerLow)
                         row.power = (PowerLow + (((PowerHigh - PowerLow) / Duration) * i)) *
                                     settings.value(QStringLiteral("ftp"), 200.0).toDouble();
@@ -123,7 +146,10 @@ QList<trainrow> zwiftworkout::load(const QByteArray &input) {
                 }
 
                 row.power = Power * settings.value(QStringLiteral("ftp"), 200.0).toDouble();
-                row.duration = QTime(Duration / 3600, Duration / 60, Duration % 60, 0);
+                if (!durationAsDistance(sportType, durationType))
+                    row.duration = QTime(Duration / 3600, Duration / 60, Duration % 60, 0);
+                else
+                    row.distance = Duration / 1000.0;
                 list.append(row);
             }
         }
