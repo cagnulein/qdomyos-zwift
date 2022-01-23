24da48f26542c29de166ecc6a6d77e9b5e5b52a3
Roberto Viola <cagnulein@gmail.com>
2022-01-23 05:40:45 +0100

    the app starts with 10 watts ,im not pedaling (Issue #607)

--- src/domyosbike.cpp
+++ src/domyosbike.cpp
@@ -672,7 +672,7 @@ uint16_t domyosbike::watts() {
     // ref
     // https://translate.google.com/translate?hl=it&sl=en&u=https://support.wattbike.com/hc/en-us/articles/115001881825-Power-Resistance-and-Cadence-Tables&prev=search&pto=aue
 
-    if (currentSpeed().value() <= 0) {
+    if (currentCadence().value() <= 0) {
         return 0;
     }
     v = wattsFromResistance(currentResistance().value());
