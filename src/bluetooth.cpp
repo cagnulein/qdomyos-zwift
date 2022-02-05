4375d74007793353b14d9aea7d4a718165ed4c50
Roberto Viola <cagnulein@gmail.com>
2022-02-05 18:18:35 +0100

    U.K. Horizon 7.0 AT 02 (Aug2020) does not connect

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -696,6 +696,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         b.name().toUpper().startsWith(QStringLiteral("T318_")) ||   // FTMS
                         b.name().toUpper().startsWith(QStringLiteral("TRX3500")) || // FTMS
                         b.name().toUpper().startsWith(QStringLiteral("JFTMPARAGON")) ||
+                        b.name().toUpper().startsWith(QStringLiteral("JFTM")) || // FTMS
                         b.name().toUpper().startsWith(QStringLiteral("ESANGLINKER"))) &&
                        !horizonTreadmill && filter) {
                 discoveryAgent->stop();
