9921796b6ce81ec743886159d898350089022d4f
Roberto Viola <Cagnulein@gmail.com>
2022-05-13 09:51:07 +0200

    trying to fix init stage

--- src/serialdircon.cpp
+++ src/serialdircon.cpp
@@ -28,7 +28,6 @@ void serialDircon::write(const uint8_t *buffer, int len, QString info) {
 
 void serialDircon::run() {
 
-    const uint8_t init0[] = {0x02, 0x68, 0x04, 0x00, 0x6c, 0x03};
     const uint8_t init1[] = {0x02, 0x68, 0x50, 0x0b, 0x11, 0x00, 0x00, 0x44, 0x00,
                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x66, 0x03};
     const uint8_t init2[] = {0x02, 0x68, 0x50, 0x13, 0x08, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0xb8, 0x6c,
@@ -75,18 +74,15 @@ void serialDircon::run() {
             if (initRequest) {
                 switch (phase) {
                 case 0:
-                    write(init0, sizeof(init0), "init0");
-                    break;
-                case 1:
                     write(init1, sizeof(init1), "init1");
                     break;
-                case 2:
+                case 1:
                     write(init2, sizeof(init2), "init2");
                     break;
-                case 3:
+                case 2:
                     write(init3, sizeof(init3), "init3");
                     break;
-                case 4:
+                case 3:
                     write(init4, sizeof(init4), "init4");
                     break;
                 default:
