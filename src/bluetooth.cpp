eda84c23cb13bb900bcff6087ee7a9fec266d616
Roberto Viola <Cagnulein@gmail.com>
2022-06-13 14:27:15 +0200

    Life fitness t5 #834

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -817,7 +817,9 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                     emit searchingStop();
                 userTemplateManager->start(shuaA5Treadmill);
                 innerTemplateManager->start(shuaA5Treadmill);
-            } else if ((b.name().toUpper().startsWith(QStringLiteral("TRUE"))) && !trueTreadmill && filter) {
+            } else if ((b.name().toUpper().startsWith(QStringLiteral("TRUE")) ||
+                        b.name().toUpper().startsWith(QStringLiteral("TREADMILL"))) &&
+                       !trueTreadmill && filter) {
                 settings.setValue(QStringLiteral("bluetooth_lastdevice_name"), b.name());
 #ifndef Q_OS_IOS
                 settings.setValue(QStringLiteral("bluetooth_lastdevice_address"), b.address().toString());
