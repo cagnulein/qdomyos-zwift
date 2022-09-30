647717cd73027e35ef1f214bfa89dbc6b687ddff
David Mason <drmason999@gmail.com>
2022-10-01 00:22:47 +0100

    #959 changed default for reading ios_peloton_workaround setting to true

--- src/flywheelbike.cpp
+++ src/flywheelbike.cpp
@@ -218,7 +218,7 @@ void flywheelbike::updateStats() {
 #ifdef Q_OS_IOS
 #ifndef IO_UNDER_QT
     bool cadence = settings.value("bike_cadence_sensor", false).toBool();
-    bool ios_peloton_workaround = settings.value("ios_peloton_workaround", false).toBool();
+    bool ios_peloton_workaround = settings.value("ios_peloton_workaround", true).toBool();
     if (ios_peloton_workaround && cadence && h && firstStateChanged) {
         h->virtualbike_setCadence(currentCrankRevolutions(), lastCrankEventTime());
         h->virtualbike_setHeartRate((uint8_t)metrics_override_heartrate());
@@ -370,7 +370,7 @@ void flywheelbike::stateChanged(QLowEnergyService::ServiceState state) {
 #ifdef Q_OS_IOS
 #ifndef IO_UNDER_QT
             bool cadence = settings.value("bike_cadence_sensor", false).toBool();
-            bool ios_peloton_workaround = settings.value("ios_peloton_workaround", false).toBool();
+            bool ios_peloton_workaround = settings.value("ios_peloton_workaround", true).toBool();
             if (ios_peloton_workaround && cadence) {
                 qDebug() << "ios_peloton_workaround activated!";
                 h = new lockscreen();
