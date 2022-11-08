66e3420abcee3a958a6000334d6aa67bdaec8dff
Roberto Viola <cagnulein@gmail.com>
2022-11-08 13:37:09 +0100

    JK fitness 577 bike #1032

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1152,6 +1152,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                 userTemplateManager->start(npeCableBike);
                 innerTemplateManager->start(npeCableBike);
             } else if (((b.name().startsWith("FS-") && hammerRacerS) ||
+                        (b.name().toUpper().startsWith("DHZ-")) ||   // JK fitness 577
                         (b.name().toUpper().startsWith("MKSM")) ||   // MKSM3600036
                         (b.name().toUpper().startsWith("YS_C1_")) || // Yesoul C1H
                         (b.name().toUpper().startsWith("DS25-")) ||  // Bodytone DS25
