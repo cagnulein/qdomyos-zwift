ad96aa5647fb44a16344a439f0597705cd0c8d67
Roberto Viola <Cagnulein@gmail.com>
2022-11-29 15:27:20 +0100

    Update ForegroundService.java

--- src/android/src/ForegroundService.java
+++ src/android/src/ForegroundService.java
@@ -27,7 +27,6 @@ public class ForegroundService extends Service {
 					 .setContentTitle("QZ is Running")
 					 .setContentText(input)
 					 .setSmallIcon(R.drawable.icon)
-					 .setLargeIcon(R.drawable.icon)
 					 .setContentIntent(pendingIntent)
 					 .build();
 					startForeground(1, notification);
