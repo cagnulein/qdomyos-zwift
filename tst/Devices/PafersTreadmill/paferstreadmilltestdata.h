e526e1e68dd917f1d4f69f42768baf96bbd8aff4
David Mason <drmason999@gmail.com>
2023-01-22 00:07:10 +0000

    #1197 added additional configurations for BH iBoxter test data

--- tst/Devices/PafersTreadmill/paferstreadmilltestdata.h
+++ tst/Devices/PafersTreadmill/paferstreadmilltestdata.h
@@ -5,10 +5,23 @@
 
 class PafersTreadmillTestData : public BluetoothDeviceTestData {
 protected:
-    bool configureSettings(DeviceDiscoveryInfo& info, bool enable) const override {
-        info.pafers_treadmill = enable;
-        return true;
+    void configureSettings(const DeviceDiscoveryInfo& info, bool enable, std::vector<DeviceDiscoveryInfo> configurations) const override {
+        DeviceDiscoveryInfo config(info);
+
+        if (enable) {
+            for(int x = 1; x<=3; x++) {
+                config.pafers_treadmill = x & 1;
+                config.pafers_treadmill_bh_iboxster_plus = x & 2;
+                configurations.push_back(config);
+            }
+        } else {
+            config.pafers_treadmill = false;
+            config.pafers_treadmill_bh_iboxster_plus = false;
+            configurations.push_back(config);
+        }
+
     }
+
 
 public:
     PafersTreadmillTestData() : BluetoothDeviceTestData("Pafers Treadmill") {
