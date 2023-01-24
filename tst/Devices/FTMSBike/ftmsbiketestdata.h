b96d3a195ec67e91db0114fb9279f41360ff869f
David Mason <drmason999@gmail.com>
2023-01-24 20:04:52 +0000

    #1197 updated comment

--- tst/Devices/FTMSBike/ftmsbiketestdata.h
+++ tst/Devices/FTMSBike/ftmsbiketestdata.h
@@ -63,7 +63,8 @@ public:
         this->addDeviceName("HAMMER ", comparison::StartsWithIgnoreCase); // HAMMER 64123
 
 
-        // Starts with DT- and is 14+ characters long. TODO: update once variable length is supported
+        // Starts with DT- and is 14+ characters long.
+        // TODO: update once addDeviceName can generate valid and invalid names for variable length patterns
         this->addDeviceName("DT-0123456789ABCD", comparison::IgnoreCase); // Sole SB700
         this->addDeviceName("DT-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", comparison::IgnoreCase); // Sole SB700
         this->addInvalidDeviceName("DT-0123456789ABC", comparison::IgnoreCase); // too short for Sole SB700
