af00b19f9942b0db277d51adfaec12ba6f7fc0d9
David Mason <drmason999@gmail.com>
2023-04-18 22:48:31 +0100

    #1222 fixed inheritance of PafersBike following last commit

--- tst/Devices/PafersBike/pafersbiketestdata.h
+++ tst/Devices/PafersBike/pafersbiketestdata.h
@@ -3,7 +3,7 @@
 #include "Devices/Bike/biketestdata.h"
 #include "pafersbike.h"
 
-class PafersBikeTestData : public BluetoothDeviceTestData {
+class PafersBikeTestData : public BikeTestData {
 protected:
     bool configureSettings(DeviceDiscoveryInfo& info, bool enable) const override {
         // the treadmill is given priority
@@ -11,7 +11,7 @@ protected:
         return true;
     }
 public:
-    PafersBikeTestData() : BluetoothDeviceTestData("Pafers Bike") {
+    PafersBikeTestData() : BikeTestData("Pafers Bike") {
         this->addDeviceName("PAFERS_", comparison::StartsWithIgnoreCase);
     }
 
