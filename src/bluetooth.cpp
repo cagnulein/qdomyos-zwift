c8cf9367dbeba959b628e04bfa654a3d5775dbd8
Roberto Viola <cagnulein@gmail.com>
2023-05-13 14:52:21 +0200

    Auto Sync Not Working On Google TV with IPAD ios #1441

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -45,12 +45,14 @@ bluetooth::bluetooth(bool logs, const QString &deviceName, bool noWriteResistanc
         if (this->discoveryAgent && !this->discoveryAgent->isActive()) {
             emit searchingStop();
         }
+        // this signal is not associated to anything in this moment, since the homeform is not loaded yet
         this->signalBluetoothDeviceConnected(pelotonBike);
     }
 
 #ifdef TEST
     schwinnIC4Bike = (schwinnic4bike *)new bike();
-    this->startTemplateManagers(schwinnIC4Bike);
+    // this signal is not associated to anything in this moment, since the homeform is not loaded yet
+    this->signalBluetoothDeviceConnected(schwinnIC4Bike);
     connectedAndDiscovered();
     return;
 #endif
@@ -87,38 +89,6 @@ bluetooth::bluetooth(bool logs, const QString &deviceName, bool noWriteResistanc
 #ifndef Q_OS_WIN
         discoveryAgent->setLowEnergyDiscoveryTimeout(10000);
 #endif
-
-#ifdef Q_OS_IOS
-        // Schwinn bikes on iOS allows to be connected to several instances, so in this way
-        // QZ will remember the address and will try to connect to it
-        QString b = settings.value(QZSettings::bluetooth_lastdevice_name, QZSettings::default_bluetooth_lastdevice_name)
-                        .toString();
-        qDebug() << "last device name (IC BIKE workaround)" << b;
-        if (!b.compare(settings.value(QZSettings::filter_device, QZSettings::default_filter_device).toString()) &&
-            (b.toUpper().startsWith("IC BIKE") || b.toUpper().startsWith("C7-"))) {
-
-            this->stopDiscovery();
-            schwinnIC4Bike = new schwinnic4bike(noWriteResistance, noHeartService);
-            // stateFileRead();
-            QBluetoothDeviceInfo bt;
-            bt.setDeviceUuid(QBluetoothUuid(
-                settings
-                    .value(QZSettings::bluetooth_lastdevice_address, QZSettings::default_bluetooth_lastdevice_address)
-                    .toString()));
-            // set name method doesn't exist
-            emit(deviceConnected(bt));
-            connect(schwinnIC4Bike, SIGNAL(connectedAndDiscovered()), this, SLOT(connectedAndDiscovered()));
-            // connect(echelonConnectSport, SIGNAL(disconnected()), this, SLOT(restart()));
-            connect(schwinnIC4Bike, SIGNAL(debug(QString)), this, SLOT(debug(QString)));
-            // connect(echelonConnectSport, SIGNAL(speedChanged(double)), this, SLOT(speedChanged(double)));
-            // connect(echelonConnectSport, SIGNAL(inclinationChanged(double)), this, SLOT(inclinationChanged(double)));
-            qDebug() << "UUID" << bt.deviceUuid();
-            schwinnIC4Bike->deviceDiscovered(bt);
-            this->signalBluetoothDeviceConnected(schwinnIC4Bike);
-            qDebug() << "connecting directly";
-        }
-#endif
-
         this->startDiscovery();
     }
 }
@@ -479,6 +449,37 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
 
         eliteSterzoSmartFound = eliteSterzoSmartAvaiable();
     }
+
+#ifdef Q_OS_IOS
+    // Schwinn bikes on iOS allows to be connected to several instances, so in this way
+    // QZ will remember the address and will try to connect to it
+    QString b = settings.value(QZSettings::bluetooth_lastdevice_name, QZSettings::default_bluetooth_lastdevice_name)
+                    .toString();
+    qDebug() << "last device name (IC BIKE workaround)" << b;
+    if (!b.compare(settings.value(QZSettings::filter_device, QZSettings::default_filter_device).toString()) &&
+        (b.toUpper().startsWith("IC BIKE") || b.toUpper().startsWith("C7-"))) {
+
+        this->stopDiscovery();
+        schwinnIC4Bike = new schwinnic4bike(noWriteResistance, noHeartService);
+        // stateFileRead();
+        QBluetoothDeviceInfo bt;
+        bt.setDeviceUuid(QBluetoothUuid(
+            settings
+                .value(QZSettings::bluetooth_lastdevice_address, QZSettings::default_bluetooth_lastdevice_address)
+                .toString()));
+        // set name method doesn't exist
+        emit(deviceConnected(bt));
+        connect(schwinnIC4Bike, SIGNAL(connectedAndDiscovered()), this, SLOT(connectedAndDiscovered()));
+        // connect(echelonConnectSport, SIGNAL(disconnected()), this, SLOT(restart()));
+        connect(schwinnIC4Bike, SIGNAL(debug(QString)), this, SLOT(debug(QString)));
+        // connect(echelonConnectSport, SIGNAL(speedChanged(double)), this, SLOT(speedChanged(double)));
+        // connect(echelonConnectSport, SIGNAL(inclinationChanged(double)), this, SLOT(inclinationChanged(double)));
+        qDebug() << "UUID" << bt.deviceUuid();
+        schwinnIC4Bike->deviceDiscovered(bt);
+        this->signalBluetoothDeviceConnected(schwinnIC4Bike);
+        qDebug() << "connecting directly";
+    }
+#endif
 
     QVector<quint16> ids = device.manufacturerIds();
     qDebug() << "manufacturerData";
