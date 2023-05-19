4451680cea9097b311de90e8f385694ef6091c17
Roberto Viola <Cagnulein@gmail.com>
2023-05-19 12:07:50 +0200

    fixing test cases for Sole treadmills

--- tst/Devices/devicediscoveryinfo.cpp
+++ tst/Devices/devicediscoveryinfo.cpp
@@ -37,6 +37,7 @@ void DeviceDiscoveryInfo::setValues(QSettings &settings, bool clear) const {
     settings.setValue(QZSettings::ftms_accessory_name, this->ftmsAccessoryName);
     settings.setValue(QZSettings::pafers_treadmill_bh_iboxster_plus, this->pafers_treadmill_bh_iboxster_plus);
     settings.setValue(QZSettings::iconcept_elliptical, this->iconcept_elliptical);
+    settings.setValue(QZSettings::sole_treadmill_inclination, this->sole_treadmill_inclination);
 }
 
 void DeviceDiscoveryInfo::getValues(QSettings &settings){
@@ -59,7 +60,7 @@ void DeviceDiscoveryInfo::getValues(QSettings &settings){
     this->toorx_bike = settings.value(QZSettings::toorx_bike, QZSettings::default_toorx_bike).toBool();
     this->toorx_ftms = settings.value(QZSettings::toorx_ftms, QZSettings::default_toorx_ftms).toBool();
     this->toorx_ftms_treadmill = settings.value(QZSettings::toorx_ftms_treadmill, QZSettings::default_toorx_ftms_treadmill).toBool();
-    this->horizon_treadmill_force_ftms = settings.value(QZSettings::horizon_treadmill_force_ftms, QZSettings::horizon_treadmill_force_ftms).toBool();
+    this->horizon_treadmill_force_ftms = settings.value(QZSettings::horizon_treadmill_force_ftms, QZSettings::default_horizon_treadmill_force_ftms).toBool();
     this->snode_bike = settings.value(QZSettings::snode_bike, QZSettings::default_snode_bike).toBool();
     this->fitplus_bike = settings.value(QZSettings::fitplus_bike, QZSettings::default_fitplus_bike).toBool();
     this->technogym_myrun_treadmill_experimental = settings.value(QZSettings::technogym_myrun_treadmill_experimental, QZSettings::default_technogym_myrun_treadmill_experimental).toBool();
@@ -68,6 +69,7 @@ void DeviceDiscoveryInfo::getValues(QSettings &settings){
     this->ftmsAccessoryName = settings.value(QZSettings::ftms_accessory_name, QZSettings::default_ftms_accessory_name).toString();
     this->pafers_treadmill_bh_iboxster_plus = settings.value(QZSettings::pafers_treadmill_bh_iboxster_plus, QZSettings::default_pafers_treadmill_bh_iboxster_plus).toBool();
     this->iconcept_elliptical = settings.value(QZSettings::iconcept_elliptical, QZSettings::default_iconcept_elliptical).toBool();
+    this->sole_treadmill_inclination = settings.value(QZSettings::sole_treadmill_inclination, QZSettings::default_sole_treadmill_inclination).toBool();
 }
 
 void DeviceDiscoveryInfo::loadDefaultValues() {
