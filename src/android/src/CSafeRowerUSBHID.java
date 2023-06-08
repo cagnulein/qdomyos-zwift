5d1b1156450695bf22a62c77b145ac2764651a53
Roberto Viola <Cagnulein@gmail.com>
2023-06-08 11:11:01 +0200

    Usbhid #1486 (#1510)
    * Revert "fixing android build"
    
    This reverts commit c44f57687645fc5c75569330e5bc0e6683bd564f.
    
    * Revert "Rowing concept2 pm3, pm4 (bluetooth) #1486 (#1505)"
    
    This reverts commit 71647a8406d752413aa003fa5f3d8e0f9d0dd294.
    
    * usb hid first commit
    
    * fixing android build
    
    * Update CSafeRowerUSBHID.java

---
+++ src/android/src/CSafeRowerUSBHID.java
@@ -0,0 +1,58 @@
+package org.cagnulen.qdomyoszwift;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Deque;
+import java.util.EnumSet;
+import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+public class CSafeRowerUSBHID {
+
+	static HidBridge hidBridge;
+	static byte[] receiveData;
+	static int lastReadLen = 0;
+
+	public static void open(Context context) {
+		 Log.d("QZ","CSafeRowerUSBHID open");
+		 hidBridge = new HidBridge(context, 0x17A4, 0x0002);
+		 boolean ret = hidBridge.OpenDevice();
+		 Log.d("QZ","hidBridge.OpenDevice " + ret);
+	}
+
+   public static void write (byte[] bytes) {
+		Log.d("QZ","CSafeRowerUSBHID writing " + new String(bytes, StandardCharsets.UTF_8));
+		hidBridge.WriteData(bytes);
+		}
+
+   public static int readLen() {
+		return lastReadLen;
+	}
+
+   public static byte[] read() {
+		receiveData = hidBridge.GetReceivedDataFromQueue();
+		lastReadLen = receiveData.length;
+		Log.d("QZ","CSafeRowerUSBHID reading " + lastReadLen + new String(receiveData, StandardCharsets.UTF_8));
+		return receiveData;
+	}
+}
