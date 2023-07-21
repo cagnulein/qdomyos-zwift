bc8302c761f056ca7133fabb11c8b5a33c6f8dd1
Roberto Viola <cagnulein@gmail.com>
2023-07-21 09:06:38 +0200

    adding virtual peloton bike to nordictrackadbbike

--- src/nordictrackifitadbbike.cpp
+++ src/nordictrackifitadbbike.cpp
@@ -205,10 +205,10 @@ void nordictrackifitadbbike::processPendingDatagrams() {
 
 #ifdef Q_OS_IOS
 #ifndef IO_UNDER_QT
-        bool cadence = settings.value(QZSettings::bike_cadence_sensor, QZSettings::default_bike_cadence_sensor).toBool();
+        bool cadencep = settings.value(QZSettings::bike_cadence_sensor, QZSettings::default_bike_cadence_sensor).toBool();
         bool ios_peloton_workaround =
             settings.value(QZSettings::ios_peloton_workaround, QZSettings::default_ios_peloton_workaround).toBool();
-        if (ios_peloton_workaround && cadence && h && firstStateChanged) {
+        if (ios_peloton_workaround && cadencep && h && firstStateChanged) {
             h->virtualbike_setCadence(currentCrankRevolutions(), lastCrankEventTime());
             h->virtualbike_setHeartRate((uint8_t)metrics_override_heartrate());
         }
