a4f7acffa85c8c41cc25e7647e5b96dd59d671f9
Roberto Viola <cagnulein@gmail.com>
2023-09-01 14:07:15 +0200

    receiving HR

--- src/android/src/MyActivity.java
+++ src/android/src/MyActivity.java
@@ -1,7 +1,19 @@
 package org.cagnulen.qdomyoszwift;
 import android.util.Log;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.Wearable;
 
-public class MyActivity extends org.qtproject.qt5.android.bindings.QtActivity {
+public class MyActivity extends org.qtproject.qt5.android.bindings.QtActivity, WearableActivity implements DataApi.DataListener {
 
  private final String TAG = "MyActivity";
 
@@ -13,5 +25,28 @@ public class MyActivity extends org.qtproject.qt5.android.bindings.QtActivity {
 	 this.getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	 activity_ = this;
 	 Log.v(TAG, "onCreate");
+
+         googleApiClient = new GoogleApiClient.Builder(this)
+                 .addApi(Wearable.API)
+                 .build();
+
+         googleApiClient.connect();
+         Wearable.DataApi.addListener(googleApiClient, this);
+ }
+
+@Override
+ public void onDataChanged(DataEventBuffer dataEvents) {
+     for (DataEvent event : dataEvents) {
+         if (event.getType() == DataEvent.TYPE_CHANGED) {
+             String path = event.getDataItem().getUri().getPath();
+             if (path.equals("/qz")) {
+                 DataMap dataMap = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
+                 int heartRate = dataMap.getInt("heart_rate");
+
+                 Log.v(TAG, heartRate);
+                 // Handle the received heart rate data here
+             }
+         }
+     }
  }
 }
