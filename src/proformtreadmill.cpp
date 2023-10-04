f0696222b8297848d43606b611dade27e9243b12
Roberto Viola <Cagnulein@gmail.com>
2023-10-04 08:11:35 +0200

    Revert "Speed and Incline not working on QZ Desktop app [BUG] #1675"
    This reverts commit 9f0111e9198efff8b9a14b28682f9876dc26b9b0.

--- src/proformtreadmill.cpp
+++ src/proformtreadmill.cpp
@@ -43,12 +43,7 @@ void proformtreadmill::writeCharacteristic(uint8_t *data, uint8_t data_len, cons
     }
     writeBuffer = new QByteArray((const char *)data, data_len);
 
-    if (gattWriteCharacteristic.properties() & QLowEnergyCharacteristic::WriteNoResponse) {
-        gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer,
-                                                             QLowEnergyService::WriteWithoutResponse);
-    } else {
-        gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer);
-    }
+    gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer);
 
     if (!disable_log) {
         emit debug(QStringLiteral(" >> ") + writeBuffer->toHex(' ') + QStringLiteral(" // ") + info);
