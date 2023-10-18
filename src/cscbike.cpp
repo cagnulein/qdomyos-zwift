64d40b5d7972c66cbd7b751dea81ace04aa472c8
Roberto Viola <Cagnulein@gmail.com>
2023-10-18 09:04:37 +0200

    Revert "Garmin Cadence Sensor not sending data (Issue #1704)"
    This reverts commit 42c2c52eef051ffee12d2ba8b66617febb9275e1.

--- src/cscbike.cpp
+++ src/cscbike.cpp
@@ -311,12 +311,8 @@ void cscbike::stateChanged(QLowEnergyService::ServiceState state) {
 
     qDebug() << QStringLiteral("all services discovered!");
 
-    QBluetoothUuid CyclingSpeedAndCadence(QBluetoothUuid::CyclingSpeedAndCadence);
-    QBluetoothUuid BatteryService(QBluetoothUuid::BatteryService);
-
     for (QLowEnergyService *s : qAsConst(gattCommunicationChannelService)) {
-        if (s->state() == QLowEnergyService::ServiceDiscovered &&
-            (s->serviceUuid() == CyclingSpeedAndCadence || s->serviceUuid() == BatteryService)) {
+        if (s->state() == QLowEnergyService::ServiceDiscovered) {
             // establish hook into notifications
             connect(s, &QLowEnergyService::characteristicChanged, this, &cscbike::characteristicChanged);
             connect(s, &QLowEnergyService::characteristicWritten, this, &cscbike::characteristicWritten);
