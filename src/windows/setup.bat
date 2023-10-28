17e5cea6ac12d0ef6829a6f63ea410d315f57ef8
Roberto Viola <Cagnulein@gmail.com>
2023-10-28 13:38:35 +0200

    Update setup.bat

--- src/windows/setup.bat
+++ src/windows/setup.bat
@@ -5,7 +5,7 @@ REM Get the current directory of the script
 set "script_dir=%~dp0"
 
 REM Check if the directory is already in the PATH
-echo !PATH! | find /i "!script_dir!" > nul
+echo !PATH! | find /i "!script_dir!python\x64\Scripts\;!script_dir!python\x64\" > nul
 if errorlevel 1 (
   REM Append the directory to the PATH variable
   set "PATH=!PATH!;!script_dir!python\x64\Scripts\;!script_dir!python\x64\"
