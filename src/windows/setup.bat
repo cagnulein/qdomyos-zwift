2336b2413764b13b0b115cd8f477cc7d00e42f43
Roberto Viola <Cagnulein@gmail.com>
2023-10-28 13:33:25 +0200

    Update setup.bat

--- src/windows/setup.bat
+++ src/windows/setup.bat
@@ -8,7 +8,7 @@ REM Check if the directory is already in the PATH
 echo !PATH! | find /i "!script_dir!" > nul
 if errorlevel 1 (
   REM Append the directory to the PATH variable
-  set "PATH=!PATH!;!script_dir!"
+  set "PATH=!PATH!;!script_dir!python\x64\Scripts\;!script_dir!python\x64\"
   echo The script directory has been added to the PATH.
 ) else (
   echo The script directory is already in the PATH.
