591629f2aa74f253dcebe9d87b7d02327f71d904
Roberto Viola <Cagnulein@gmail.com>
2023-11-30 08:35:46 +0100

    Update ci_post_clone.sh

--- build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
+++ build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 brew install qt@5
-sed -i '' '/5E618435888B9D49F8540165 \/\* Qt Qmake \*\//d' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj
-sed -i '' 's|/Users/cagnulein/qdomyos-zwift|..|g' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj
-sed -i '' 's|/Users/cagnulein/Qt/5.15.2/ios/|/usr/local/opt/qt@5/|g' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj
+sed -i '' '/5E618435888B9D49F8540165 \/\* Qt Qmake \*\//d' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj/project.pbxproj
+sed -i '' 's|/Users/cagnulein/qdomyos-zwift|..|g' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj/project.pbxproj
+sed -i '' 's|/Users/cagnulein/Qt/5.15.2/ios/|/usr/local/opt/qt@5/|g' /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj/project.pbxproj
+
