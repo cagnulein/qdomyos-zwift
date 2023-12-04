f5bc0ca05ef50c0ca5b06666c70f74ac19e86d4d
Roberto Viola <Cagnulein@gmail.com>
2023-12-04 14:42:56 +0100

    Update ci_post_clone.sh

--- build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
+++ build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
@@ -21,10 +21,10 @@ qmake
 make -j8
 make install
 
-mkdir /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/QtHttpServer
-cp -a /usr/local/Cellar/qt@5/5.15.10_1/lib/QtHttpServer.framework/Versions/5/Headers/*.* /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/QtHttpServer/
+mkdir /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer
+cp -a /usr/local/Cellar/qt@5/5.15.10_1/lib/QtHttpServer.framework/Versions/5/Headers/*.* /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer/
 
-ls -ltr /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/QtHttpServer/
+ls -ltr /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer/
 
 git reset -- /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj/project.pbxproj
 
