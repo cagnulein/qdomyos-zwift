b07ae8922a43275d9d2a21889025eb01426c3708
Roberto Viola <Cagnulein@gmail.com>
2023-12-05 11:19:51 +0100

    Update ci_post_clone.sh

--- build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
+++ build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,4 @@
-curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/67e7aa02813056543f2d31dcd643e7418eb329cb/Formula/qt%405.rb
-ls -ltr
-brew install --HEAD -s QtAT5.rb
+brew install qt@5
 #awk '
 #    /\/\* Begin PBXShellScriptBuildPhase section \*\// {p=1}
 #    /\/\* End PBXShellScriptBuildPhase section \*\// {p=0; next}
@@ -13,6 +11,8 @@ brew install --HEAD -s QtAT5.rb
 
 PATH="/usr/local/opt/qt@5/bin:$PATH"
 
+ls -ltr /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.0.sdk/System/Library/Frameworks/OpenGL.framework/Headers
+
 cd /Volumes/workspace/repository/src/qthttpserver 
 qmake
 make -j8
@@ -23,12 +23,12 @@ qmake -spec macx-ios-clang CONFIG+=iphoneos CONFIG+=device CONFIG+=qml_debug -af
 make -j8
 make install
 
-mkdir /usr/local/Cellar/qt@5/5.15.2_1/include/QtHttpServer
-mkdir /usr/local/Cellar/qt@5/5.15.2_1/include/QtSslServer
-cp -a /usr/local/Cellar/qt@5/5.15.2_1/lib/QtHttpServer.framework/Versions/5/Headers/* /usr/local/Cellar/qt@5/5.15.2_1/include/QtHttpServer/
-cp -a /usr/local/Cellar/qt@5/5.15.2_1/lib/QtSslServer.framework/Versions/5/Headers/* /usr/local/Cellar/qt@5/5.15.2_1/include/QtSslServer/
+mkdir /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer
+mkdir /usr/local/Cellar/qt@5/5.15.10_1/include/QtSslServer
+cp -a /usr/local/Cellar/qt@5/5.15.10_1/lib/QtHttpServer.framework/Versions/5/Headers/* /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer/
+cp -a /usr/local/Cellar/qt@5/5.15.10_1/lib/QtSslServer.framework/Versions/5/Headers/* /usr/local/Cellar/qt@5/5.15.10_1/include/QtSslServer/
 
-ls -ltr /usr/local/Cellar/qt@5/5.15.2_1/include/QtHttpServer/
+ls -ltr /usr/local/Cellar/qt@5/5.15.10_1/include/QtHttpServer/
 
 git reset -- /Volumes/workspace/repository/build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/qdomyoszwift.xcodeproj/project.pbxproj
 
