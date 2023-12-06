eb2b9f202fd3009bc99ddbbea00b90987f52c4e0
Roberto Viola <Cagnulein@gmail.com>
2023-12-06 08:43:09 +0100

    Revert "Update qfit.cpp"
    This reverts commit 9d5d43d542b363e93d0d159e77f63630fc953e17.

--- src/qfit.cpp
+++ src/qfit.cpp
@@ -280,7 +280,7 @@ void qfit::save(const QString &filename, QList<SessionLine> session, bluetoothde
 
         fit::RecordMesg newRecord;
         sl = session.at(i);
-        fit::DateTime dateI((time_t)session.at(i).time.toSecsSinceEpoch());
+        fit::DateTime date((time_t)session.at(i).time.toSecsSinceEpoch());
         newRecord.SetHeartRate(sl.heart);
         uint8_t cad = sl.cadence;
         if (powr_sensor_running_cadence_half_on_strava)
@@ -314,7 +314,7 @@ void qfit::save(const QString &filename, QList<SessionLine> session, bluetoothde
         // using just the start point as reference in order to avoid pause time
         // strava ignore the elapsed field
         // this workaround could leads an accuracy issue.
-        newRecord.SetTimestamp(dateI.GetTimeStamp());
+        newRecord.SetTimestamp(date);
         encode.Write(newRecord);
 
         if (sl.lapTrigger) {
@@ -328,8 +328,8 @@ void qfit::save(const QString &filename, QList<SessionLine> session, bluetoothde
 
             encode.Write(lapMesg);
 
-            lapMesg.SetStartTime(dateI.GetTimeStamp());
-            lapMesg.SetTimestamp(dateI.GetTimeStamp());
+            lapMesg.SetStartTime(date.GetTimeStamp() + i);
+            lapMesg.SetTimestamp(date.GetTimeStamp() + i);
             lapMesg.SetEvent(FIT_EVENT_WORKOUT);
             lapMesg.SetEventType(FIT_EVENT_LAP);
         }
