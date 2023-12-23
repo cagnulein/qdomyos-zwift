56c112ed4c59b97473a58ce33a0b81ec2b9b8d48
Al Udell <63697253+victorypoint@users.noreply.github.com>
2023-12-23 05:02:10 -0400

    Update python scripts to use AI.Server for OCR (#1847)
    * Update python scripts to use AI.Server for OCR
    
    * change python from 3.10 to 3.7
    
    * Update main.yml
    
    * renaming script files
    
    * resuming here the old script as well
    
    * adding new build for ai server in CI
    
    * renaming python scripts for AI.server
    
    * Update homeform.cpp
    
    * Update homeform.cpp
    
    * fixing secrets on PR
    
    * restoring...
    
    * Update main.yml
    
    * Update homeform.cpp
    
    * Update main.yml
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_workout_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_workout_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_workout_paddleocr_thread.cpp
    
    * Update main.yml
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_workout_paddleocr_thread.cpp
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update main.yml
    
    * Update main.yml
    
    * Update windows_zwift_incline_paddleocr_thread.cpp
    
    * Update windows_zwift_workout_paddleocr_thread.cpp
    
    * Update main.yml
    
    * removing nopython build for ai.server
    
    * Update main.yml
    
    ---------
    
    Co-authored-by: Roberto Viola <cagnulein@gmail.com>

--- src/homeform.cpp
+++ src/homeform.cpp
@@ -53,7 +53,7 @@ using namespace std::chrono_literals;
 #if defined(WIN32)
 #pragma message("DEFINE STRAVA_CLIENT_ID!!!")
 #else
-#warning "DEFINE STRAVA_CLIENT_ID!!!"
+#pragma message "DEFINE STRAVA_CLIENT_ID!!!"
 #endif
 #endif
 #define _STR(x) #x
@@ -5930,7 +5930,7 @@ QOAuth2AuthorizationCodeFlow *homeform::strava_connect() {
 #elif defined(WIN32)
 #pragma message("DEFINE STRAVA_SECRET_KEY!!!")
 #else
-#warning "DEFINE STRAVA_SECRET_KEY!!!"
+#pragma message "DEFINE STRAVA_SECRET_KEY!!!"
 #endif
     strava->setModifyParametersFunction(
         buildModifyParametersFunction(QUrl(QLatin1String("")), QUrl(QLatin1String(""))));
@@ -6051,7 +6051,7 @@ void homeform::sendMail() {
     SmtpClient smtp(STRINGIFY(SMTP_SERVER), 587, SmtpClient::TlsConnection);
     connect(&smtp, SIGNAL(smtpError(SmtpClient::SmtpError)), this, SLOT(smtpError(SmtpClient::SmtpError)));
 #else
-#warning "stmp server is unset!"
+#pragma message "stmp server is unset!"
     SmtpClient smtp(QLatin1String(""), 25, SmtpClient::TlsConnection);
     return;
 #endif
@@ -6063,7 +6063,7 @@ void homeform::sendMail() {
 #define STRINGIFY(x) _STR(x)
     smtp.setUser(STRINGIFY(SMTP_USERNAME));
 #else
-#warning "smtp username is unset!"
+#pragma message "smtp username is unset!"
     return;
 #endif
 #ifdef SMTP_PASSWORD
@@ -6071,7 +6071,7 @@ void homeform::sendMail() {
 #define STRINGIFY(x) _STR(x)
     smtp.setPassword(STRINGIFY(SMTP_PASSWORD));
 #else
-#warning "smtp password is unset!"
+#pragma message "smtp password is unset!"
     return;
 #endif
 
