f32dcb4e1e7d4e7fd464a94f22e50fa330c5c4bb
Roberto Viola <Cagnulein@gmail.com>
2023-12-25 17:32:11 +0000

    Floating window: adding total remaining time after the elapsed time (Issue #1863)

--- src/templateinfosenderbuilder.cpp
+++ src/templateinfosenderbuilder.cpp
@@ -1064,7 +1064,7 @@ void TemplateInfoSenderBuilder::buildContext(bool forceReinit) {
             obj.setProperty(QStringLiteral("power_zone_lapavg"), ((bike *)device)->currentPowerZone().lapAverage());
             obj.setProperty(QStringLiteral("power_zone_lapmax"), ((bike *)device)->currentPowerZone().lapMax());
             obj.setProperty(QStringLiteral("target_power_zone"), ((bike *)device)->targetPowerZone().value());
-            obj.setProperty(QStringLiteral("power_zone_color"), homeform::singleton()->target_zone->valueFontColor());
+            obj.setProperty(QStringLiteral("power_zone_color"), homeform::singleton()->ftp->valueFontColor());
             obj.setProperty(QStringLiteral("peloton_resistance"),
                             (dep = ((bike *)device)->pelotonResistance()).value());
             obj.setProperty(QStringLiteral("peloton_resistance_avg"), dep.average());
