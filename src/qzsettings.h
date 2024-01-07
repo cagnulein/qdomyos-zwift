0a03267ceac533b1c481d9c06b3de496d38b5c39
Roberto Viola <cagnulein@gmail.com>
2024-01-07 09:34:43 +0100

    That the treadmill always has a minimum slope (Issue #1960)

--- src/qzsettings.h
+++ src/qzsettings.h
@@ -1953,6 +1953,12 @@ class QZSettings {
     static const QString proform_8_5_treadmill;
     static constexpr int default_proform_8_5_treadmill = false;
 
+    static const QString treadmill_incline_min;
+    static constexpr float treadmill_incline_min = -100;
+
+    static const QString treadmill_incline_max;
+    static constexpr float default_treadmill_incline_max = 100;
+
     /**
      * @brief Write the QSettings values using the constants from this namespace.
      * @param showDefaults Optionally indicates if the default should be shown with the key.
