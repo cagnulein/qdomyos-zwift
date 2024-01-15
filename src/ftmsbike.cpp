ac6468e5078f25cefe21cf246ba60e3ef24d345f
Roberto Viola <cagnulein@gmail.com>
2024-01-15 10:20:28 +0100

    Power Profile for BodyTone du30 #1981

--- src/ftmsbike.cpp
+++ src/ftmsbike.cpp
@@ -98,7 +98,7 @@ void ftmsbike::forcePower(int16_t requestPower) {
 
 uint16_t ftmsbike::wattsFromResistance(double resistance) {
     if(DU30_bike) {
-        double y = 1.46193548 * Cadence.value() + 0.0000887836638 * Cadence.value() * resistance + 0.000625 * resistance * resistance + 0.0580645161 * x + 0.00292986091 * resistance + 6.48448135542904;
+        double y = 1.46193548 * Cadence.value() + 0.0000887836638 * Cadence.value() * resistance + 0.000625 * resistance * resistance + 0.0580645161 * Cadence.value() + 0.00292986091 * resistance + 6.48448135542904;
         return y;
     }
     return 1;
