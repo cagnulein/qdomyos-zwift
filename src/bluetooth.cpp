1d85e90b15aa77135151e7322e7fec8c995330b7
Roberto Viola <cagnulein@gmail.com>
2024-01-27 07:21:18 +0100

    Bowflex Treadmill 7 #2057

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1226,6 +1226,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         !b.name().compare(ftms_treadmill, Qt::CaseInsensitive) ||
                         (b.name().toUpper().startsWith(QStringLiteral("DOMYOS-TC")) && deviceHasService(b, QBluetoothUuid((quint16)0x1826))) ||
                         b.name().toUpper().startsWith(QStringLiteral("XT685")) ||
+                        b.name().toUpper().startsWith(QStringLiteral("T118_")) ||
                         b.name().toUpper().startsWith(QStringLiteral("FIT-")) ||                            // FIT-1596
                         b.name().toUpper().startsWith(QStringLiteral("MOBVOI TM")) ||                        // FTMS
                         b.name().toUpper().startsWith(QStringLiteral("TUNTURI T60-")) ||                     // FTMS
