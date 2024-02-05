bc8443e84d57ff544ee2879b4420dd1c5bfae9d7
Roberto Viola <cagnulein@gmail.com>
2024-02-05 15:01:08 +0100

    Update zapBleUuids.swift

--- src/ios/zwift_play/zapBleUuids.swift
+++ src/ios/zwift_play/zapBleUuids.swift
@@ -12,25 +12,3 @@ enum ZapBleUuids {
     // I have not seen this characteristic used. Guess it could be for Device Firmware Update (DFU)? it is a chip from Nordic.
     static let zwiftUnknown6CharacteristicUUID = CBUUID(string: "00000006-19CA-4651-86E5-FA29DCDD09D1")
 }
-
-enum GenericBleUuids {
-    static let genericAccessServiceUUID = CBUUID(string: BleUuids.uuidFromShortString("1800"))
-    static let deviceNameCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A00")) // Zwift Play
-    static let appearanceCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A01")) // [964] Gamepad (HID Subtype)
-    static let preferredConnectionParamsCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A04"))
-    static let centralAddressResolutionCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2AA6"))
-
-    static let genericAttributeServiceUUID = CBUUID(string: BleUuids.uuidFromShortString("1801"))
-    static let serviceChangedCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A05"))
-
-    static let deviceInformationServiceUUID = CBUUID(string: BleUuids.uuidFromShortString("180A"))
-    static let manufacturerNameStringCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A29")) // Zwift Inc.
-    static let serialNumberStringCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A25")) // 02-1[MAC]
-    static let hardwareRevisionStringCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A27")) // B.0
-    static let firmwareRevisionStringCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A26")) // 1.1.0
-
-    static let batteryServiceUUID = CBUUID(string: BleUuids.uuidFromShortString("180F"))
-    static let batteryLevelCharacteristicUUID = CBUUID(string: BleUuids.uuidFromShortString("2A19")) // 89
-
-    static let defaultDescriptorUUID = CBUUID(string: BleUuids.uuidFromShortString("2902"))
-}
