73e9e44c86ae469b73438a3e558aabc7315f0b7e
Roberto Viola <Cagnulein@gmail.com>
2024-02-05 17:31:00 +0100

    Update encryptionUtils.swift

--- src/ios/zwift_play/encryptionUtils.swift
+++ src/ios/zwift_play/encryptionUtils.swift
@@ -1,32 +1,38 @@
-import CryptoKit
 import Foundation
+import CryptoSwift
 
-@available(iOS 14.0, *)
-enum EncryptionUtils {
+struct EncryptionUtils {
     static let keyLength = 32
     static let hkdfLength = 36
     static let macLength = 4
 
-    // Converti una chiave pubblica P-256 in Data concatenando le coordinate X e Y
+    // Converti una chiave pubblica EC in array di byte
     static func publicKeyToByteArray(publicKey: P256.KeyAgreement.PublicKey) -> Data {
+        // Assumendo che `publicKey` sia una rappresentazione simile di CryptoKit's P256,
+        // la conversione diretta alla rappresentazione di byte è già disponibile
         return publicKey.rawRepresentation
     }
 
-    // Genera una chiave pubblica P-256 da Data
+    // Genera una chiave pubblica EC dai byte ricevuti
     static func generatePublicKey(publicKeyBytes: Data) throws -> P256.KeyAgreement.PublicKey {
+        // Assumendo l'uso di CryptoKit per la generazione della chiave pubblica
         return try P256.KeyAgreement.PublicKey(x963Representation: publicKeyBytes)
     }
 
-    // Genera byte del segreto condiviso utilizzando ECDH
+    // Genera byte di segreto condiviso
     static func generateSharedSecretBytes(privateKey: P256.KeyAgreement.PrivateKey, publicKey: P256.KeyAgreement.PublicKey) -> Data {
         let sharedSecret = try! privateKey.sharedSecretFromKeyAgreement(with: publicKey)
+        // Direttamente in formato Data
         return sharedSecret.withUnsafeBytes { Data($0) }
     }
 
-    // Utilizza HKDF per derivare chiavi da un segreto condiviso
-    static func generateHKDFBytes(secretKey: Data, salt: Data) -> SymmetricKey {
-        let symmetricKey = SymmetricKey(data: secretKey)
-        let hkdfDerivedBytes = HKDF<SHA256>.deriveKey(inputKeyMaterial: symmetricKey, salt: salt, info: Data(), outputByteCount: hkdfLength)
-        return hkdfDerivedBytes
+    // Genera byte usando HKDF
+    static func generateHKDFBytes(secretKey: Data, salt: Data) -> Data {
+        // Utilizzo di HKDF in CryptoSwift
+        let keyMaterial = secretKey.bytes
+        let saltBytes = salt.bytes
+        let hkdf = HKDF(key: keyMaterial, salt: saltBytes, variant: .sha2(.sha256))
+        let derivedKey = try! hkdf.calculate(info: [], outputLength: hkdfLength)
+        return Data(derivedKey)
     }
 }
