5bc6e7aa6fbd9c11a38dbbe32080f7598bf55550
David Mason <drmason999@gmail.com>
2024-02-08 22:50:03 +0000

    #855 added web link

--- tst/main.cpp
+++ tst/main.cpp
@@ -3,12 +3,13 @@
 #include <QApplication>
 #include <QTimer>
 
+// https://forum.qt.io/topic/84229/is-there-a-canonical-way-to-set-up-qapplication-and-google-test-together/2
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app{argc, argv};
 
-    QTimer::singleShot(500, [&]()
+    QTimer::singleShot(0, [&]()
     {
         ::testing::InitGoogleTest(&argc, argv);
         auto testResult = RUN_ALL_TESTS();
