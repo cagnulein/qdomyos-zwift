acdcae018ff1e9575ca24789420452b699851114
David Mason <drmason999@gmail.com>
2024-02-10 19:37:12 +0000

    #614 moved settings information to QSZettings

--- src/virtualdevices/virtualdevice.cpp
+++ src/virtualdevices/virtualdevice.cpp
@@ -1,26 +1,24 @@
 #include "virtualdevice.h"
+#include "qzsettings.h"
 #include <QSettings>
+#include <QDebug>
 
 const QString virtualdevice::DomyosBridge = QStringLiteral("DomyosBridge");
 const QString virtualdevice::VirtualDeviceBaseName = QStringLiteral("QZ");
-const QString virtualdevice::VirtualDeviceAlternativeNameSuffixSettingsKey = QStringLiteral("virtual_device_alt_name_suffix");
-const QString virtualdevice::VirtualDeviceAlternativeNameEnabledSettingsKey = QStringLiteral("virtual_device_alt_name_enabled");
 
 QString virtualdevice::get_VirtualDeviceName() {
     QSettings settings;
 
-    bool usingSuffix = settings.value(VirtualDeviceAlternativeNameEnabledSettingsKey, false).toBool();
+    bool usingSuffix = settings.value(QZSettings::virtual_device_alt_name_enabled, QZSettings::default_virtual_device_alt_name_enabled).toBool();
 
     if(!usingSuffix)
         return DomyosBridge;
 
-    QString suffix = settings.value(virtualdevice::VirtualDeviceAlternativeNameSuffixSettingsKey, "0" ).toString();
+    QString suffix = settings.value(QZSettings::virtual_device_alt_name_suffix, QZSettings::default_virtual_device_alt_name_suffix).toString();
 
     return VirtualDeviceBaseName+suffix;
 }
 
-#include "virtualdevice.h"
-#include <QDebug>
 
 virtualdevice::virtualdevice(QObject *parent)
     : QObject{parent}
