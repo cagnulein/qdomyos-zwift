c803d7253e0b2a307b3f92fde229513f8f7263cb
Roberto Viola <cagnulein@gmail.com>
2024-02-13 16:19:17 +0100

    KICKR CORE with only power characteristic handled
    https://github.com/cagnulein/qdomyos-zwift/commit/f184b2b545044e035f4fc98809e9af9e469c994e#commitcomment-138579511

--- tst/Devices/FTMSBike/ftmsbiketestdata.h
+++ tst/Devices/FTMSBike/ftmsbiketestdata.h
@@ -54,7 +54,7 @@ public:
         this->addDeviceName("ZWIFT HUB", comparison::StartsWithIgnoreCase);
         this->addDeviceName("MAGNUS ", comparison::StartsWithIgnoreCase); // Saris Trainer
         this->addDeviceName("FLXCY-", comparison::StartsWithIgnoreCase); // Pro FlexBike        
-        this->addDeviceName("KICKR CORE", comparison::StartsWithIgnoreCase);
+        //this->addDeviceName("KICKR CORE", comparison::StartsWithIgnoreCase);
         this->addDeviceName("B94", comparison::StartsWithIgnoreCase);
         this->addDeviceName("DBF135", comparison::IgnoreCase);
         this->addDeviceName("STAGES BIKE", comparison::StartsWithIgnoreCase);
