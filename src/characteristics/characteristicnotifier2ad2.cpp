43f1bda372fe2b5527d2c498abf79a19c72f9344
Roberto Viola <cagnulein@gmail.com>
2024-02-21 11:17:55 +0100

    Elliptical trainer "FAL-SPORTS0195" (Brand and model: Maxxus CX 5.1) won't connect correctly #2025
    https://github.com/cagnulein/qdomyos-zwift/issues/2025#issuecomment-1956212492

--- src/characteristics/characteristicnotifier2ad2.cpp
+++ src/characteristics/characteristicnotifier2ad2.cpp
@@ -14,6 +14,11 @@ int CharacteristicNotifier2AD2::notify(QByteArray &value) {
     bool virtual_device_rower =
         settings.value(QZSettings::virtual_device_rower, QZSettings::default_virtual_device_rower).toBool();
     bool rowerAsABike = !virtual_device_rower && dt == bluetoothdevice::ROWING;
+    bool double_cadence = settings.value(QZSettings::powr_sensor_running_cadence_double, QZSettings::default_powr_sensor_running_cadence_double).toBool();
+    double cadence_multiplier = 2.0;
+    if (double_cadence)
+        cadence_multiplier = 1.0;
+
 
     double normalizeWattage = Bike->wattsMetric().value();
     if (normalizeWattage < 0)
@@ -27,8 +32,8 @@ int CharacteristicNotifier2AD2::notify(QByteArray &value) {
         value.append((char)(normalizeSpeed & 0xFF));      // speed
         value.append((char)(normalizeSpeed >> 8) & 0xFF); // speed
 
-        value.append((char)((uint16_t)(Bike->currentCadence().value() * 2) & 0xFF));        // cadence
-        value.append((char)(((uint16_t)(Bike->currentCadence().value() * 2) >> 8) & 0xFF)); // cadence
+        value.append((char)((uint16_t)(Bike->currentCadence().value() * cadence_multiplier) & 0xFF));        // cadence
+        value.append((char)(((uint16_t)(Bike->currentCadence().value() * cadence_multiplier) >> 8) & 0xFF)); // cadence
 
         value.append((char)Bike->currentResistance().value()); // resistance
         value.append((char)(0));                               // resistance
@@ -40,11 +45,6 @@ int CharacteristicNotifier2AD2::notify(QByteArray &value) {
         value.append((char)0);                            // Bkool FTMS protocol HRM offset 1280 fix
         return CN_OK;
     } else if (dt == bluetoothdevice::TREADMILL || dt == bluetoothdevice::ELLIPTICAL || dt == bluetoothdevice::ROWING) {
-        QSettings settings;
-        bool double_cadence = settings.value(QZSettings::powr_sensor_running_cadence_double, QZSettings::default_powr_sensor_running_cadence_double).toBool();
-        double cadence_multiplier = 2.0;
-        if (double_cadence)
-            cadence_multiplier = 1.0;
         uint16_t normalizeSpeed = (uint16_t)qRound(Bike->currentSpeed().value() * 100);
         value.append((char)0x64); // speed, inst. cadence, resistance lvl, instant power
         value.append((char)0x02); // heart rate
