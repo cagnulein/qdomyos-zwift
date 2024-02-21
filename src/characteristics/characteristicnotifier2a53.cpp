33ca41a871dfff5e58ee50337b81747b32b0fdcc
Roberto Viola <cagnulein@gmail.com>
2024-02-21 16:41:25 +0100

    Update characteristicnotifier2a53.cpp

--- src/characteristics/characteristicnotifier2a53.cpp
+++ src/characteristics/characteristicnotifier2a53.cpp
@@ -5,13 +5,18 @@ CharacteristicNotifier2A53::CharacteristicNotifier2A53(bluetoothdevice *Bike, QO
     : CharacteristicNotifier(0x2a53, parent), Bike(Bike) {}
 
 int CharacteristicNotifier2A53::notify(QByteArray &value) {
+    QSettings settings;
+    bool double_cadence = settings.value(QZSettings::powr_sensor_running_cadence_double, QZSettings::default_powr_sensor_running_cadence_double).toBool();
+    double cadence_multiplier = 1.0;
+    if (double_cadence)
+        cadence_multiplier = 2.0;    
     bluetoothdevice::BLUETOOTH_TYPE dt = Bike->deviceType();
     value.append(0x02); // total distance
     uint16_t speed = Bike->currentSpeed().value() / 3.6 * 256;
     uint32_t distance = Bike->odometer() * 10000.0;
     value.append((char)((speed & 0xFF)));
     value.append((char)((speed >> 8) & 0xFF));
-    value.append((char)(Bike->currentCadence().value() / 2.0));
+    value.append((char)(Bike->currentCadence().value() / cadence_multiplier));
     value.append((char)((distance & 0xFF)));
     value.append((char)((distance >> 8) & 0xFF));
     value.append((char)((distance >> 16) & 0xFF));
