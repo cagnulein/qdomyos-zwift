56b5e759a61c579c634b994d233889fe2e0582b1
Roberto Viola <Cagnulein@gmail.com>
2024-02-26 09:45:34 +0100

    target zone vs ftp zone (Discussion #2138)

--- src/devices/trxappgateusbbike/trxappgateusbbike.cpp
+++ src/devices/trxappgateusbbike/trxappgateusbbike.cpp
@@ -1170,7 +1170,22 @@ uint16_t trxappgateusbbike::wattsFromResistance(double resistance) {
             - 0.036 * resistance * resistance;
         return P;
     } else {
-        qDebug() << "no power table for this bike";
+        // toorx srx 500 #2138
+        double intercept = -1.0174902288995327;
+        double coef_x1 = -0.2596697;
+        double coef_x2 = -1.63400699;
+        double coef_x1_squared = 0.01471623;
+        double coef_x2_squared = 0.21051417;
+        double coef_x1_x2 = -0.08509624;
+
+        double power = intercept +
+                       coef_x1 * Cadence.value() +
+                       coef_x2 * resistance +
+                       coef_x1_squared * Cadence.value() * Cadence.value() +
+                       coef_x2_squared * resistance * resistance +
+                       coef_x1_x2 * Cadence.value() * resistance;
+        return power;
+        //qDebug() << "no power table for this bike";
     }
 }
 
