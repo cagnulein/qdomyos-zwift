09d4715084e88502d2b84f19fcf8c75cd8f860e5
Roberto Viola <Cagnulein@gmail.com>
2024-03-02 12:45:01 +0100

    Update fakebike.h

--- src/devices/fakebike/fakebike.h
+++ src/devices/fakebike/fakebike.h
@@ -28,6 +28,7 @@
 #include <QString>
 
 #include "devices/bike.h"
+#include "ergtable.h"
 
 #ifdef Q_OS_IOS
 #include "ios/lockscreen.h"
@@ -38,6 +39,7 @@ class fakebike : public bike {
   public:
     fakebike(bool noWriteResistance, bool noHeartService, bool noVirtualDevice);
     bool connected() override;
+    resistance_t maxResistance() override { return 100; }
 
   private:
     QTimer *refresh;
@@ -57,6 +59,8 @@ class fakebike : public bike {
 
     uint16_t oldLastCrankEventTime = 0;
     uint16_t oldCrankRevs = 0;
+    
+    ergTable _ergTable;
 
 #ifdef Q_OS_IOS
     lockscreen *h = 0;
