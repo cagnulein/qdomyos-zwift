da4c159f7ed58de873d13093679c20e24b24d534
Roberto Viola <Cagnulein@gmail.com>
2024-03-13 09:17:00 +0100

    test it

--- src/ios/ios_app_delegate.mm
+++ src/ios/ios_app_delegate.mm
@@ -21,8 +21,43 @@
               [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
           };
       }];
+    [self setupDynamicQuickActions];
 
     return YES;
+}
+
+- (void)setupDynamicQuickActions {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths firstObject];
+    NSString *profilesDirectory = [documentsDirectory stringByAppendingPathComponent:@"profiles"];
+    
+    NSError *error;
+    NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:profilesDirectory error:&error];
+    if (error) {
+        NSLog(@"Error reading files: %@", error.localizedDescription);
+        return;
+    }
+    
+    NSMutableArray *quickActions = [NSMutableArray array];
+    
+    for (NSString *fileName in files) {
+        UIApplicationShortcutIcon *icon = [UIApplicationShortcutIcon iconWithType:UIApplicationShortcutIconTypeCompose];
+        UIApplicationShortcutItem *shortcutItem = [[UIApplicationShortcutItem alloc] initWithType:[@"org.cagnulein.qdomyoszwift." stringByAppendingString:fileName]
+                                                                                   localizedTitle:fileName
+                                                                                localizedSubtitle:nil
+                                                                                             icon:icon
+                                                                                         userInfo:nil];
+        [quickActions addObject:shortcutItem];
+    }
+    
+    [UIApplication sharedApplication].shortcutItems = quickActions;
+}
+
+- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void(^)(BOOL succeeded))completionHandler {
+    if ([shortcutItem.type hasPrefix:@"com.yourapp."]) {
+        NSString *fileName = [shortcutItem.type stringByReplacingOccurrencesOfString:@"org.cagnulein.qdomyoszwift." withString:@""];
+        //self.selectedShortcutItem = fileName;
+    }
 }
 
 - (void)application:(UIApplication *)application
