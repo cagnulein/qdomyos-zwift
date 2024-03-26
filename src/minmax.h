2ab31e8bad196b4855ae5dd39041f861704dd8ef
David Mason <drmason999@gmail.com>
2024-03-26 00:01:42 +0000

    #2174 avoid template in cpp

--- src/minmax.h
+++ src/minmax.h
@@ -6,14 +6,21 @@ struct minmax {
   private:
     T _max, _min;
   public:
-    const T max();
-    const T min();
+    const T max() { return _max; }
+    const T min() { return _min; }
 
-    explicit minmax(const T min, const T max);
+    explicit minmax(const T min, const T max) :  _max(max), _min(min) {}
 
-    T clip(const T value) const;
+    T clip(const T value) const {
+        T result = value;
+        if(value<_min) result=_min;
+        else if(value>_max) result=_max;
+        return result;
+    }
 
-    bool contains(const T value) const;
+    bool contains(const T value) const {
+        return value>=_min && value<=_max;
+    }
 };
 
 #endif
--- src/qdomyos-zwift.pri
+++ src/qdomyos-zwift.pri
@@ -74,7 +74,6 @@ DEFINES += QT_DEPRECATED_WARNINGS IO_UNDER_QT SMTP_BUILD NOMINMAX
 SOURCES += \
     $$PWD/devices/focustreadmill/focustreadmill.cpp \
     $$PWD/devices/trxappgateusbelliptical/trxappgateusbelliptical.cpp \
-    $$PWD/minmax.cpp \
 QTelnet.cpp \
 devices/bkoolbike/bkoolbike.cpp \
 devices/csaferower/csafe.cpp \
