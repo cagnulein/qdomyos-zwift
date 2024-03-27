dbb6b32b0313e4f4ad18f557f7f5a9092124bf1d
Roberto Viola <Cagnulein@gmail.com>
2024-03-27 11:04:28 +0100

    My interest is the creation of personalized workouts for threadmill and smart bike and use QZ app standalone #2242

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1426,6 +1426,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         (b.name().toUpper().startsWith("BIKE-")) ||
                         (b.name().toUpper().startsWith("SPAX-BK-")) ||
                         (b.name().toUpper().startsWith("YSV1")) ||
+                        (b.name().toUpper().startsWith("ZYCLEZBIKE")) ||
                         (b.name().toUpper().startsWith("DOMYOS-BIKING-")) ||
                         (b.name().toUpper().startsWith("ICSE") && b.name().length() == 4) ||
                         (b.name().toUpper().startsWith("CSRB") && b.name().length() == 11) ||
