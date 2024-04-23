d37978431c564f267fe0841f24c205dae98cd113
Roberto Viola <Cagnulein@gmail.com>
2024-04-23 10:15:23 +0200

    Kingsmith Walkingpad P1 no cadence reported #2304

--- src/devices/kingsmithr1protreadmill/kingsmithr1protreadmill.h
+++ src/devices/kingsmithr1protreadmill/kingsmithr1protreadmill.h
@@ -51,6 +51,7 @@ class kingsmithr1protreadmill : public treadmill {
 
     double GetSpeedFromPacket(const QByteArray &packet);
     double GetTargetSpeedFromPacket(const QByteArray &packet);
+    double GetStepsFromPacket(const QByteArray &packet);
     double GetInclinationFromPacket(const QByteArray &packet);
     double GetKcalFromPacket(const QByteArray &packet);
     double GetDistanceFromPacket(const QByteArray &packet);
@@ -66,8 +67,10 @@ class kingsmithr1protreadmill : public treadmill {
     bool searchStopped = false;
     uint8_t sec1Update = 0;
     uint8_t firstInit = 0;
+    double lastStepCount = 0;
     QByteArray lastPacket;
     QDateTime lastTimeCharacteristicChanged;
+    QDateTime lastTimeStepCountChanged;
     bool firstCharacteristicChanged = true;
 
     QTimer *refresh;
