139630fcb85405ec82fdce384b8ad92dfbf92f48
Roberto Viola <Cagnulein@gmail.com>
2024-06-05 16:45:22 +0200

    fixing crash on quit on ios or android

--- src/devices/nordictrackifitadbtreadmill/nordictrackifitadbtreadmill.cpp
+++ src/devices/nordictrackifitadbtreadmill/nordictrackifitadbtreadmill.cpp
@@ -509,18 +509,18 @@ bool nordictrackifitadbtreadmill::connected() { return true; }
 void nordictrackifitadbtreadmill::stopLogcatAdbThread() {
     qDebug() << "stopLogcatAdbThread()";
     
+#ifdef Q_OS_WIN32
     initiateThreadStop();
     logcatAdbThread->quit();
     logcatAdbThread->terminate();
     
-#ifdef Q_OS_WIN32
     QProcess process;
     QString command = "/c wmic process where name='adb.exe' delete";
     process.start("cmd.exe", QStringList(command.split(' ')));
     process.waitForFinished(-1); // will wait forever until finished
-#endif
     
     logcatAdbThread->wait();
+#endif
 }
 
 void nordictrackifitadbtreadmill::initiateThreadStop() {
