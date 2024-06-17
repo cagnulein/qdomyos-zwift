78df32ea8d480dc66fe4b67f471f6d2d0257db51
Roberto Viola <Cagnulein@gmail.com>
2024-06-17 09:57:51 +0200

    Cross Rope #2381

--- src/devices/crossrope/crossrope.cpp
+++ src/devices/crossrope/crossrope.cpp
@@ -58,7 +58,12 @@ void crossrope::writeCharacteristic(uint8_t *data, uint8_t data_len, const QStri
     }
     writeBuffer = new QByteArray((const char *)data, data_len);
 
-    gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer);
+    if (gattWriteCharacteristic.properties() & QLowEnergyCharacteristic::WriteNoResponse) {
+        gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer,
+                                                             QLowEnergyService::WriteWithoutResponse);
+    } else {
+        gattCommunicationChannelService->writeCharacteristic(gattWriteCharacteristic, *writeBuffer);
+    }
 
     if (!disable_log) {
         emit debug(QStringLiteral(" >> ") + writeBuffer->toHex(' ') +
