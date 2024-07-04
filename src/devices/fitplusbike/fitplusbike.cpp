70f5dc2cc4a0d7e94bb729654e5130950a8143f3
Roberto Viola <Cagnulein@gmail.com>
2024-07-04 15:22:31 +0200

    BH Osaka bike #2425

--- src/devices/fitplusbike/fitplusbike.cpp
+++ src/devices/fitplusbike/fitplusbike.cpp
@@ -563,7 +563,29 @@ void fitplusbike::characteristicChanged(const QLowEnergyCharacteristic &characte
             return;
 
         if (newValue.length() == 15) {
-            Resistance = newValue.at(5);
+            resistance_t res = newValue.at(5);
+            if (settings.value(QZSettings::gears_from_bike, QZSettings::default_gears_from_bike).toBool()) {
+                qDebug() << QStringLiteral("gears_from_bike") << res << Resistance.value() << gears()
+                         << lastRawRequestedResistanceValue << lastRequestedResistance().value();
+                if (
+                     // if the resistance is different from the previous one
+                    res != qRound(Resistance.value()) &&
+                    // and the last target resistance is different from the current one or there is no any pending last
+                    // requested resistance
+                    ((lastRequestedResistance().value() != res && lastRequestedResistance().value() != 0) ||
+                     lastRawRequestedResistanceValue == -1) &&
+                    // and the difference between the 2 resistances are less than 6
+                    qRound(Resistance.value()) > 1 && qAbs(res - qRound(Resistance.value())) < 6) {
+
+                    int8_t g = gears();
+                    g += (res - qRound(Resistance.value()));
+                    qDebug() << QStringLiteral("gears_from_bike APPLIED") << gears() << g;
+                    lastRawRequestedResistanceValue = -1; // in order to avoid to change resistance with the setGears
+                    setGears(g);
+                }
+            }
+            Resistance = res;
+            emit resistanceRead(Resistance.value());
             if (merach_MRK || sportstech_sx600) {
                 // if we change this, also change the wattsFromResistance function. We can create a standard function in
                 // order to have all the costants in one place (I WANT MORE TIME!!!)
