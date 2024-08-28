328f0992b6bf9b362d45c1bc914bc48969bb04c1
Roberto Viola <Cagnulein@gmail.com>
2024-08-28 08:52:57 +0200

    Kettler Spin Bike Racer S #2542

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1548,6 +1548,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                 this->signalBluetoothDeviceConnected(horizonGr7Bike);
             } else if ((b.name().toUpper().startsWith(QStringLiteral("STAGES ")) ||
                         (b.name().toUpper().startsWith("TACX SATORI")) ||
+                        (b.name().toUpper().startsWith("RACER S")) ||
                         ((b.name().toUpper().startsWith("KICKR CORE")) && !deviceHasService(b, QBluetoothUuid((quint16)0x1826)) && deviceHasService(b, QBluetoothUuid((quint16)0x1818))) ||
                         (b.name().toUpper().startsWith(QStringLiteral("QD")) && b.name().length() == 2) ||
                         (b.name().toUpper().startsWith(QStringLiteral("DFC")) && b.name().length() == 3) ||
