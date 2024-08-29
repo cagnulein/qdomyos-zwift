7e3eab5b8c88f94cd695c01ef66132d255fa4703
Roberto Viola <cagnulein@gmail.com>
2024-08-29 21:24:21 +0200

    New echelon stride treadmill #2555

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1628,7 +1628,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
             } else if ((b.name().toUpper().startsWith(QLatin1String("ECH-STRIDE")) ||
                         b.name().toUpper().startsWith(QLatin1String("ECH-UK-")) ||
                         b.name().toUpper().startsWith(QLatin1String("ECH-FR-")) ||
-                        b.name().toUpper().startsWith(QLatin1String("STRIDE-")) ||
+                        b.name().toUpper().startsWith(QLatin1String("STRIDE")) ||
                         b.name().toUpper().startsWith(QLatin1String("STRIDE6S-")) ||
                         b.name().toUpper().startsWith(QLatin1String("ECH-SD-SPT"))) &&
                        !echelonStride && filter) {
