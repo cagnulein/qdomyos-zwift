bce2d2605c6d35731c389d7bc1020e6288c5a79c
Roberto Viola <Cagnulein@gmail.com>
2024-09-07 07:23:13 +0200

    WalkingPad MC21 treadmill #2580

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1331,6 +1331,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         b.name().toUpper().startsWith(QStringLiteral("LJJ-")) ||                            // LJJ-02351A
                         b.name().toUpper().startsWith(QStringLiteral("WLT-EP-")) ||                             // Flow elliptical
                         (b.name().toUpper().startsWith("SCHWINN 810")) ||
+                        b.name().toUpper().startsWith(QStringLiteral("KS-MC")) ||    
                         (b.name().toUpper().startsWith(QStringLiteral("KS-HD-Z1D"))) ||                     // Kingsmith WalkingPad Z1
                         (b.name().toUpper().startsWith(QStringLiteral("FIT-")) && deviceHasService(b, QBluetoothUuid((quint16)0x1826))) || // sports tech f37s treadmill #2412
                         (b.name().toUpper().startsWith(QStringLiteral("NOBLEPRO CONNECT")) && deviceHasService(b, QBluetoothUuid((quint16)0x1826))) || // FTMS
