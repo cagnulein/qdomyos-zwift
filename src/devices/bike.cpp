114ee5317a7e0723009524919b1f873c664bd02a
Roberto Viola <cagnulein@gmail.com>
2024-09-13 11:32:45 +0200

    Several Issues Using QZ with Rouvy and Zwift Play Controllers #2541

--- src/devices/bike.cpp
+++ src/devices/bike.cpp
@@ -86,7 +86,12 @@ void bike::changePower(int32_t power) {
 double bike::gears() { return m_gears; }
 void bike::setGears(double gears) {
     QSettings settings;
+    bool gears_zwift_ratio = settings.value(QZSettings::gears_zwift_ratio, QZSettings::default_gears_zwift_ratio).toBool();
     qDebug() << "setGears" << gears;
+    if(gears_zwift_ratio && (gears > 24 || gears < 1)) {
+        qDebug() << "new gear value ignored because of gears_zwift_ratio setting!";
+        return;
+    }
     m_gears = gears;
     settings.setValue(QZSettings::gears_current_value, m_gears);
     if (lastRawRequestedResistanceValue != -1) {
