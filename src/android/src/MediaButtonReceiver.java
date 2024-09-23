3b2fd006375cdb90a54c3c5411ec230f3cf6c862
Roberto Viola <Cagnulein@gmail.com>
2024-09-23 11:22:50 +0200

    Update MediaButtonReceiver.java

--- src/android/src/MediaButtonReceiver.java
+++ src/android/src/MediaButtonReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.view.KeyEvent;
+import android.content.IntentFilter;
+import android.util.Log;
 
 public class MediaButtonReceiver extends BroadcastReceiver {
     @Override
