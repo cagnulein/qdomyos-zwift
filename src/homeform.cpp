96b199aa9864635cf2e0b76949d7dbaf1fbc6061
Roberto Viola <Cagnulein@gmail.com>
2024-10-30 16:41:04 +0100

    Update homeform.cpp

--- src/homeform.cpp
+++ src/homeform.cpp
@@ -135,6 +135,11 @@ homeform::homeform(QQmlApplicationEngine *engine, bluetooth *bl) {
 #ifdef Q_OS_ANDROID
     m_locationServices = QAndroidJniObject::callStaticMethod<jboolean>("org/cagnulen/qdomyoszwift/LocationHelper", "start",
                                               "(Landroid/content/Context;)Z", QtAndroid::androidContext().object());
+    if(m_locationServices) {
+        QSettings settings;
+        // so if someone pressed the skip message but now he forgot to enable GPS it will prompt out
+        settings.setValue(QZSettings::skipLocationServicesDialog, QZSettings::default_skipLocationServicesDialog);
+    }
 #endif
 
 #ifdef Q_OS_IOS
