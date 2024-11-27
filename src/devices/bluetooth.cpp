70f2f2ecb54ae8cf65d8cc385cc0cd979b549f56
Roberto Viola <Cagnulein@gmail.com>
2024-11-27 09:23:44 +0100

    NEO BIKE SMART added

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1563,6 +1563,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         (b.name().toUpper().startsWith("JUSTO")) ||
                         (b.name().toUpper().startsWith("MYCYCLE ")) ||
                         (b.name().toUpper().startsWith("T2 ")) ||
+                        (b.name().toUpper().startsWith("NEO BIKE SMART")) ||
                         (b.name().toUpper().startsWith("ZDRIVE")) ||
                         (b.name().toUpper().startsWith("KICKR MOVE ")) ||
                         (b.name().toUpper().startsWith("T300P_")) ||
