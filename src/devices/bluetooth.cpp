bad290d104fe6b548ffbe99a3389b641f59017d2
Roberto Viola <Cagnulein@gmail.com>
2024-12-09 10:24:04 +0100

    Data fields not updating Trojan Pro Spin Bike 2.0, iConsole #2884

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1584,6 +1584,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         (b.name().toUpper().startsWith("MYCYCLE ")) ||
                         (b.name().toUpper().startsWith("T2 ")) ||
                         (b.name().toUpper().startsWith("RC-MAX-")) ||
+                        (b.name().toUpper().startsWith("TPS-SPBIKE-2.0")) ||
                         (b.name().toUpper().startsWith("NEO BIKE SMART")) ||
                         (b.name().toUpper().startsWith("ZDRIVE")) ||
                         (b.name().toUpper().startsWith("T300P_")) ||
