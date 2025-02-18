47dff20d115989e14d6b5b758f1f91a244a3a28e
Roberto Viola <cagnulein@gmail.com>
2025-02-18 16:20:20 +0100

    Row and Tread Target Pace Issues from 2.18.19 Update #3206

--- src/peloton.cpp
+++ src/peloton.cpp
@@ -1634,9 +1634,10 @@ void peloton::performance_onfinish(QNetworkReply *reply) {
                         paceintensity_upper = oo[QStringLiteral("upper")].toInt();
                         paceintensity_avg = ((paceintensity_upper - paceintensity_lower) / 2.0) + paceintensity_lower;
                         if(paceintensity_lower < 7) {
-                            speed_lower = treadmill_pace[paceintensity_lower].levels[peloton_treadmill_level].speed;
-                            speed_upper = treadmill_pace[paceintensity_upper].levels[peloton_treadmill_level].speed;
+                            speed_lower = treadmill_pace[paceintensity_lower].levels[peloton_treadmill_level].slow_pace;
+                            speed_upper = treadmill_pace[paceintensity_upper].levels[peloton_treadmill_level].fast_pace;
                             speed_average = (((speed_upper - speed_lower) / 2.0) + speed_lower) * miles;
+                            miles = 1; // the pace intensity are always in km/h
                         }
                     }
                 }
