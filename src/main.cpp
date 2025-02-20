483fd87ee5e33cac0409f2c88f356206dfa7eac4
Roberto Viola <Cagnulein@gmail.com>
2025-02-20 20:03:09 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -600,7 +600,7 @@ int main(int argc, char *argv[]) {
     qInstallMessageHandler(myMessageOutput);
     qDebug() << QStringLiteral("version ") << app->applicationVersion();
     foreach (QString s, settings.allKeys()) {
-        if (!s.contains(QStringLiteral("password")) && !s.contains("user_email") && !s.contains("username")) {
+        if (!s.contains(QStringLiteral("password")) && !s.contains("user_email") && !s.contains("username") && !s.contains("token")) {
 
             qDebug() << s << settings.value(s);
         }
