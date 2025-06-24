7cf05276e95d4c00d4e1584e0122099564ab99b7
Roberto Viola <Cagnulein@gmail.com>
2025-06-24 11:32:41 +0200

    BODY BIKE Smart+ integration

--- src/devices/bluetooth.cpp
+++ src/devices/bluetooth.cpp
@@ -1792,6 +1792,7 @@ void bluetooth::deviceDiscovered(const QBluetoothDeviceInfo &device) {
                         ((b.name().toUpper().startsWith("KU")) && b.name().length() == 2) ||
                         (b.name().toUpper().startsWith("ELITETRAINER")) ||
                         (b.name().toUpper().startsWith("TOUR 600")) ||
+                        (b.name().toUpper().startsWith("SMART+ #")) ||
                         (b.name().toUpper().startsWith(QStringLiteral("QD")) && b.name().length() == 2) ||
                         (b.name().toUpper().startsWith(QStringLiteral("RM")) && b.name().length() == 2) ||
                         (b.name().toUpper().startsWith(QStringLiteral("DR")) && b.name().length() == 2) ||
