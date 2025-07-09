f11ebdbcc8efad4a9ea4352931741663d15cdffb
Roberto Viola <Cagnulein@gmail.com>
2025-07-09 16:54:30 +0200

    Update ci_pre_xcodebuild.sh

--- build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_pre_xcodebuild.sh
+++ build-qdomyos-zwift-Qt_5_15_2_for_iOS-Debug/ci_scripts/ci_pre_xcodebuild.sh
@@ -95,7 +95,7 @@ fi
 # CRITICAL: Run make to compile Qt project and generate MOC files
 echo "Running make to compile Qt project and generate MOC files..."
 # Use parallel compilation for faster builds
-make -f Makefile.qdomyos-zwift-lib -j$(sysctl -n hw.ncpu)
+make -f src/Makefile.qdomyos-zwift-lib -j$(sysctl -n hw.ncpu)
 
 echo "make completed successfully - MOC files generated"
 
