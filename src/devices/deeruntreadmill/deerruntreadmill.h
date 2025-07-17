9f5a2ae12062d0290fd939a8c55fc8fc70ac0aad
Roberto Viola <Cagnulein@gmail.com>
2025-07-17 20:37:17 +0200

    DeerRun Treadmill integration #2621 (#3547)

--- src/devices/deeruntreadmill/deerruntreadmill.h
+++ src/devices/deeruntreadmill/deerruntreadmill.h
@@ -48,6 +48,7 @@ class deerruntreadmill : public treadmill {
     void btinit(bool startTape);
     void writeCharacteristic(const QLowEnergyCharacteristic characteristic, uint8_t *data, uint8_t data_len,
                              const QString &info, bool disable_log = false, bool wait_for_response = false);
+    void writeUnlockCharacteristic(uint8_t *data, uint8_t data_len, const QString &info, bool disable_log = false);
     void startDiscover();
     uint8_t calculateXOR(uint8_t arr[], size_t size);
     bool noConsole = false;
@@ -66,6 +67,11 @@ class deerruntreadmill : public treadmill {
     QLowEnergyService *gattCommunicationChannelService = nullptr;
     QLowEnergyCharacteristic gattWriteCharacteristic;
     QLowEnergyCharacteristic gattNotifyCharacteristic;
+    
+    QLowEnergyService *unlock_service = nullptr;
+    QLowEnergyCharacteristic unlock_characteristic;
+    
+    bool pitpat = false;
 
     bool initDone = false;
     bool initRequest = false;
