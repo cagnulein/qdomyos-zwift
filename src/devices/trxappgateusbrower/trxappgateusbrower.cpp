3d9c3e410344f16da6d8ab0dabe3a3688634cd07
Roberto Viola <Cagnulein@gmail.com>
2025-10-08 12:00:02 +0200

    Recently I managed to decode the Rowing data from Merach NovaRow R50, are any guideline I can follow in order to add it to the app? (Discussion #3593)

--- src/devices/trxappgateusbrower/trxappgateusbrower.cpp
+++ src/devices/trxappgateusbrower/trxappgateusbrower.cpp
@@ -88,8 +88,16 @@ void trxappgateusbrower::update() {
                 }
                 requestResistance = -1;
             } else {
-                uint8_t noOpData1[] = {0xf0, 0xa2, 0x01, 0xe7, 0x7a};
-                writeCharacteristic(noOpData1, sizeof(noOpData1), QStringLiteral("noOp"));
+                // Send polling command based on device type
+                if (mearch_novarow_r50) {
+                    // Mearch NovaRow R50: request rowing data every 2 seconds (500ms * 4 = 2000ms)
+                    uint8_t pollData[] = {0xf0, 0xa2, 0x01, 0xe8, 0x7b};
+                    writeCharacteristic(pollData, sizeof(pollData), QStringLiteral("mearch_poll"));
+                } else {
+                    // Original I-CONSOLE+ polling
+                    uint8_t noOpData1[] = {0xf0, 0xa2, 0x01, 0xe7, 0x7a};
+                    writeCharacteristic(noOpData1, sizeof(noOpData1), QStringLiteral("noOp"));
+                }
             }
         }
 
@@ -164,18 +172,52 @@ void trxappgateusbrower::characteristicChanged(const QLowEnergyCharacteristic &c
         return;
     }
 
-    Resistance = newValue.at(18) - 1;
-    Speed = GetSpeedFromPacket(newValue);
-    Cadence = (GetCadenceFromPacket(newValue) * cadence_gain) + cadence_offset;
-    m_watt = GetWattFromPacket(newValue);
-    if (watts())
-        KCal += ((((0.048 * ((double)watts()) + 1.19) * weight * 3.5) / 200.0) /
-                 (60000.0 / ((double)lastRefreshCharacteristicChanged.msecsTo(
-                                QDateTime::currentDateTime())))); //(( (0.048* Output in watts +1.19) * body weight in
-                                                                  // kg * 3.5) / 200 ) / 60
-    // KCal = (((uint16_t)((uint8_t)newValue.at(15)) << 8) + (uint16_t)((uint8_t) newValue.at(14)));
-    Distance += ((Speed.value() / 3600000.0) *
-                 ((double)lastRefreshCharacteristicChanged.msecsTo(QDateTime::currentDateTime())));
+    // Parse data based on device type
+    if (mearch_novarow_r50) {
+        // Mearch NovaRow R50 data format parsing
+        // elapsedSecond = (values[4] - 1) * 60 + values[5] - 1
+        int elapsedSeconds = (newValue.at(4) - 1) * 60 + (newValue.at(5) - 1);
+
+        // strokeCount = (values[6] - 1) * 99 + (values[7] - 1)
+        int strokeCount = (newValue.at(6) - 1) * 99 + (newValue.at(7) - 1);
+
+        // strokePerMinute = (values[8] - 1) * 99 + (values[9] - 1)
+        Cadence = ((newValue.at(8) - 1) * 99 + (newValue.at(9) - 1)) * cadence_gain + cadence_offset;
+
+        // distance = (values[10] - 1) * 99 + (values[11] - 1)
+        Distance = (newValue.at(10) - 1) * 99 + (newValue.at(11) - 1);
+
+        // calories = (values[12] - 1) * 99 + (values[13] - 1)
+        KCal = (newValue.at(12) - 1) * 99 + (newValue.at(13) - 1);
+
+        // heartbeat = (values[14] - 1) * 99 + (values[15] - 1)
+        // Heart rate handled below
+
+        // power = (values[16] - 1) * 99 + (values[17] - 1)
+        m_watt = (newValue.at(16) - 1) * 99 + (newValue.at(17) - 1);
+
+        // gear = values[20] - 1
+        Resistance = newValue.at(20) - 1;
+
+        // Speed calculation based on distance/time (approximate)
+        if (elapsedSeconds > 0) {
+            Speed = (Distance.value() / elapsedSeconds) * 3.6; // convert m/s to km/h
+        }
+    } else {
+        // Original I-CONSOLE+ data format
+        Resistance = newValue.at(18) - 1;
+        Speed = GetSpeedFromPacket(newValue);
+        Cadence = (GetCadenceFromPacket(newValue) * cadence_gain) + cadence_offset;
+        m_watt = GetWattFromPacket(newValue);
+        if (watts())
+            KCal += ((((0.048 * ((double)watts()) + 1.19) * weight * 3.5) / 200.0) /
+                     (60000.0 / ((double)lastRefreshCharacteristicChanged.msecsTo(
+                                    QDateTime::currentDateTime())))); //(( (0.048* Output in watts +1.19) * body weight in
+                                                                      // kg * 3.5) / 200 ) / 60
+        // KCal = (((uint16_t)((uint8_t)newValue.at(15)) << 8) + (uint16_t)((uint8_t) newValue.at(14)));
+        Distance += ((Speed.value() / 3600000.0) *
+                     ((double)lastRefreshCharacteristicChanged.msecsTo(QDateTime::currentDateTime())));
+    }
 
     lastRefreshCharacteristicChanged = QDateTime::currentDateTime();
 
@@ -204,27 +246,44 @@ void trxappgateusbrower::characteristicChanged(const QLowEnergyCharacteristic &c
 
 void trxappgateusbrower::btinit() {
 
-    uint8_t initData1[] = {0xf0, 0xa0, 0x01, 0x00, 0x91};
-    uint8_t initData2[] = {0xf0, 0xa0, 0x01, 0xe7, 0x78};
-    uint8_t initData3[] = {0xf0, 0xa1, 0x01, 0xe7, 0x79};
-    uint8_t initData4[] = {0xf0, 0xa3, 0x01, 0xe7, 0x01, 0x7c};
-    uint8_t initData5[] = {0xf0, 0xa4, 0x01, 0xe7, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x83};
-    uint8_t initData6[] = {0xf0, 0xa5, 0x01, 0xe7, 0x02, 0x7f};
+    // Check if device is Mearch NovaRow R50 based on Bluetooth name
+    if (bluetoothDevice.name().toUpper().startsWith(QStringLiteral("MRK-R11S-"))) {
+        mearch_novarow_r50 = true;
 
-    writeCharacteristic(initData1, sizeof(initData1), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData1, sizeof(initData1), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData4, sizeof(initData4), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData5, sizeof(initData5), QStringLiteral("init"), false, true);
-    writeCharacteristic(initData6, sizeof(initData6), QStringLiteral("init"), false, true);
+        // Mearch NovaRow R50 initialization sequence to switch to Bluetooth mode
+        uint8_t initData1[] = {0xf0, 0xa5, 0x44, 0x01, 0x04, 0xde};
+        uint8_t initData2[] = {0xf0, 0xa0, 0x44, 0x01, 0xd5};
+        uint8_t initData3[] = {0xf0, 0xa0, 0x01, 0xe8, 0x79};
+        uint8_t initData4[] = {0xf0, 0xa5, 0x01, 0xe8, 0x02, 0x80};
+
+        writeCharacteristic(initData1, sizeof(initData1), QStringLiteral("mearch_init1"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("mearch_init2"), false, true);
+        writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("mearch_init3"), false, true);
+        writeCharacteristic(initData4, sizeof(initData4), QStringLiteral("mearch_init4"), false, true);
+    } else {
+        // Original I-CONSOLE+ initialization
+        uint8_t initData1[] = {0xf0, 0xa0, 0x01, 0x00, 0x91};
+        uint8_t initData2[] = {0xf0, 0xa0, 0x01, 0xe7, 0x78};
+        uint8_t initData3[] = {0xf0, 0xa1, 0x01, 0xe7, 0x79};
+        uint8_t initData4[] = {0xf0, 0xa3, 0x01, 0xe7, 0x01, 0x7c};
+        uint8_t initData5[] = {0xf0, 0xa4, 0x01, 0xe7, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x83};
+        uint8_t initData6[] = {0xf0, 0xa5, 0x01, 0xe7, 0x02, 0x7f};
+
+        writeCharacteristic(initData1, sizeof(initData1), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData1, sizeof(initData1), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData3, sizeof(initData3), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData2, sizeof(initData2), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData4, sizeof(initData4), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData5, sizeof(initData5), QStringLiteral("init"), false, true);
+        writeCharacteristic(initData6, sizeof(initData6), QStringLiteral("init"), false, true);
+    }
 
     initDone = true;
 }
