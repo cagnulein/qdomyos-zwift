f21f31b55cebbe9ba4d3271f300ac398c4fbaf5d
Your Name <your.email@example.com>
2025-10-10 16:55:36 +0100

    Tweaks for cadence

--- src/devices/antlinux/ant_broadcaster.py
+++ src/devices/antlinux/ant_broadcaster.py
@@ -117,7 +117,7 @@ class AntBroadcaster:
                 current_cadence = self._cadence_spm
 
             # Calculate how many strides should have occurred in the last time slice (dt).
-            stride_rate_sps = (current_cadence_spm / 2.0) / 60.0
+            stride_rate_sps = (current_cadence / 2.0) / 60.0
             self._stride_accumulator += strides_this_tick
 
             # If the accumulator has passed a whole number, increment the stride count
