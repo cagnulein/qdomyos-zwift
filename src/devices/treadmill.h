b45ca3e596a1a5fb4a5c5fba845bba33eca472d1
Roberto Viola <Cagnulein@gmail.com>
2025-12-17 08:34:20 +0100

    Yesoul Walking Pad steps showing as 0 in QZ (Issue #3924) (#3925)
    * Yesoul Walking Pad steps showing as 0 in QZ (Issue #3924)
    
    * Update treadmill.h

--- src/devices/treadmill.h
+++ src/devices/treadmill.h
@@ -45,6 +45,7 @@ class treadmill : public bluetoothdevice {
     static double treadmillInclinationOverride(double Inclination);
     static double treadmillInclinationOverrideReverse(double Inclination);
     void cadenceFromAppleWatch();
+    double calculateCadenceFromSpeed(double speed);
     virtual bool canHandleSpeedChange() { return true; }
     virtual bool canHandleInclineChange() { return true; }
     double runningStressScore();
@@ -91,11 +92,11 @@ class treadmill : public bluetoothdevice {
     void parseSpeed(double speed);
     void parseInclination(double speed);
     bool areInclinationSettingsDefault();
+    void evaluateStepCount();
 
   private:
     bool simulateInclinationWithSpeed();
     bool followPowerBySpeed();
-    void evaluateStepCount();
 };
 
 #endif // TREADMILL_H
