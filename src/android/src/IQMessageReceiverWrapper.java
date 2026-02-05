1af9da34e0abf07e80507acd02c0188008615b66
Roberto Viola <cagnulein@gmail.com>
2026-02-05 21:18:46 +0100

    Add comprehensive diagnostic logging to Garmin wrapper
    This logging will help diagnose the crash by showing:
    - All Intent extras and their types
    - Parcelable deserialization attempts
    - Exact point where exceptions occur
    - Full stack traces
    
    This will confirm if the INCOMING_MESSAGE fix is sufficient or if
    there are other underlying issues causing the crash.

--- src/android/src/IQMessageReceiverWrapper.java
+++ src/android/src/IQMessageReceiverWrapper.java
@@ -20,34 +20,73 @@ public class IQMessageReceiverWrapper extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        QLog.d(TAG, "onReceive intent " + intent.getAction());
-        if ("com.garmin.android.connectiq.SEND_MESSAGE_STATUS".equals(intent.getAction())) {
-            replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
-        } else if ("com.garmin.android.connectiq.OPEN_APPLICATION".equals(intent.getAction())) {
-            replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_OPEN_APPLICATION_DEVICE");
-        } else if ("com.garmin.android.connectiq.DEVICE_STATUS".equals(intent.getAction())) {
-            replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
-        } else if ("com.garmin.android.connectiq.INCOMING_MESSAGE".equals(intent.getAction())) {
-            replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
-        }
-
         try {
+            QLog.d(TAG, "=== GARMIN INTENT DEBUG START ===");
+            QLog.d(TAG, "Action: " + intent.getAction());
+
+            // Log all extras in the intent
+            if (intent.getExtras() != null) {
+                QLog.d(TAG, "Extras bundle: " + intent.getExtras());
+                try {
+                    for (String key : intent.getExtras().keySet()) {
+                        Object value = intent.getExtras().get(key);
+                        QLog.d(TAG, "  Extra[" + key + "] = " + value + " (type: " + (value != null ? value.getClass().getName() : "null") + ")");
+                    }
+                } catch (Exception e) {
+                    QLog.e(TAG, "Error iterating extras: " + e.toString());
+                }
+            } else {
+                QLog.d(TAG, "No extras in intent");
+            }
+
+            // Process known actions
+            if ("com.garmin.android.connectiq.SEND_MESSAGE_STATUS".equals(intent.getAction())) {
+                QLog.d(TAG, "Processing SEND_MESSAGE_STATUS");
+                replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
+            } else if ("com.garmin.android.connectiq.OPEN_APPLICATION".equals(intent.getAction())) {
+                QLog.d(TAG, "Processing OPEN_APPLICATION");
+                replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_OPEN_APPLICATION_DEVICE");
+            } else if ("com.garmin.android.connectiq.DEVICE_STATUS".equals(intent.getAction())) {
+                QLog.d(TAG, "Processing DEVICE_STATUS");
+                replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
+            } else if ("com.garmin.android.connectiq.INCOMING_MESSAGE".equals(intent.getAction())) {
+                QLog.d(TAG, "Processing INCOMING_MESSAGE");
+                replaceIQDeviceById(intent, "com.garmin.android.connectiq.EXTRA_REMOTE_DEVICE");
+            } else {
+                QLog.d(TAG, "Unknown action, no processing");
+            }
+
+            QLog.d(TAG, "Calling wrapped receiver.onReceive()");
             receiver.onReceive(context, intent);
+            QLog.d(TAG, "=== GARMIN INTENT DEBUG END (success) ===");
+
         } catch (Exception e) {
-            QLog.d(TAG, "Exception in Garmin message receiver: " + e.toString());
+            QLog.e(TAG, "=== EXCEPTION in wrapper (BEFORE or DURING receiver call) ===");
+            QLog.e(TAG, "Exception type: " + e.getClass().getName());
+            QLog.e(TAG, "Exception message: " + e.getMessage());
+            QLog.e(TAG, "Stack trace:", e);
+            QLog.e(TAG, "=== GARMIN INTENT DEBUG END (error) ===");
         }
     }
 
     private static void replaceIQDeviceById(Intent intent, String extraName) {
         try {
+            QLog.d(TAG, "  Attempting to get Parcelable for extra: " + extraName);
             IQDevice device = intent.getParcelableExtra(extraName);
             if (device != null) {
-//                Logger.logDebug("Replacing " + device.describeContents() + " " + device.getFriendlyName() + " by " + device.getDeviceIdentifier() );
-                intent.putExtra(extraName, device.getDeviceIdentifier());
+                QLog.d(TAG, "  Found IQDevice: " + device.getFriendlyName() + " (ID: " + device.getDeviceIdentifier() + ")");
+                long deviceId = device.getDeviceIdentifier();
+                intent.putExtra(extraName, deviceId);
+                QLog.d(TAG, "  Replaced IQDevice with Long ID: " + deviceId);
+            } else {
+                QLog.d(TAG, "  Extra '" + extraName + "' is null or not an IQDevice");
             }
         } catch (ClassCastException e) {
-            QLog.d(TAG, e.toString());
-            // It's already a long, i.e. on the simulator.
+            QLog.d(TAG, "  ClassCastException for '" + extraName + "': " + e.toString());
+            QLog.d(TAG, "  (Extra is already a Long, probably on simulator)");
+        } catch (Exception e) {
+            QLog.e(TAG, "  Unexpected exception in replaceIQDeviceById: " + e.toString());
+            QLog.e(TAG, "  Stack trace:", e);
         }
     }
 
