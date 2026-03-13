a0b9c554c05cf96f6acc422e80807f809a3c819d
Roberto Viola <Cagnulein@gmail.com>
2026-03-13 15:16:20 +0100

    Update qzsettings.cpp

--- src/qzsettings.cpp
+++ src/qzsettings.cpp
@@ -1064,7 +1064,7 @@ const QString QZSettings::step_gain = QStringLiteral("step_gain");
 const QString QZSettings::proform_carbon_tlx_treadmill = QStringLiteral("proform_carbon_tlx_treadmill");
 
 
-const uint32_t allSettingsCount = 866;
+const uint32_t allSettingsCount = 867;
 
 QVariant allSettings[allSettingsCount][2] = {
     {QZSettings::cryptoKeySettingsProfiles, QZSettings::default_cryptoKeySettingsProfiles},
