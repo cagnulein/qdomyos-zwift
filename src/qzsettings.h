0ae31ffbca62dc10da8dd1e54eb20788e622bbc9
Roberto Viola <cagnulein@gmail.com>
2026-03-15 07:05:13 +0100

    Proform Carbon TL (PFTL59723.6)

--- src/qzsettings.h
+++ src/qzsettings.h
@@ -2925,6 +2925,8 @@ class QZSettings {
 
     static const QString proform_carbon_tlx_treadmill;
     static constexpr bool default_proform_carbon_tlx_treadmill = false;
+    static const QString proform_carbon_tl_PFTL59723_6;
+    static constexpr bool default_proform_carbon_tl_PFTL59723_6 = false;
 
     /**
      * @brief Write the QSettings values using the constants from this namespace.
