3537e4436a7b6ba6875e7bfd7e3e8cd5d069506d
Roberto Viola <Cagnulein@gmail.com>
2026-04-16 11:08:06 +0200

    Increase max resistance for FS-YK bike to 24

--- src/devices/ftmsbike/ftmsbike.cpp
+++ src/devices/ftmsbike/ftmsbike.cpp
@@ -1989,7 +1989,7 @@ void ftmsbike::deviceDiscovered(const QBluetoothDeviceInfo &device) {
             FS_YK = true;
             resistance_lvl_mode = true;
             ergModeSupported = false; // this bike doesn't have ERG mode natively
-            max_resistance = 21;
+            max_resistance = 24;
         } else if(device.name().toUpper().startsWith("S18")) {
             qDebug() << QStringLiteral("S18 found");
             S18 = true;
