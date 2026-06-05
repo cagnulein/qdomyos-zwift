91d76f055fc6260d76f1699679f4e00cb69902c8
Roberto Viola <Cagnulein@gmail.com>
2026-06-05 08:41:22 +0200

    pid hr on zone 1
    https://github.com/cagnulein/qdomyos-zwift/issues/4480#issuecomment-4623844109

--- src/homeform.cpp
+++ src/homeform.cpp
@@ -7884,7 +7884,8 @@ void homeform::update() {
                             pid_heart_zone_small_inc_counter = 0;
                         } else if (currentSpeed < maxSpeed && trainprogram_pid_pushy) {
                             pid_heart_zone_small_inc_counter++;
-                            if (fabs(((float)zone) - currentHRZone) < 0.5 && pid_heart_zone_small_inc_counter > (10 * fabs(((float)zone) - currentHRZone))) {
+                            double distanceToNextZone = ((double)zone + 1.0) - currentHRZone;
+                            if (distanceToNextZone > 0.0 && pid_heart_zone_small_inc_counter > (10 * distanceToNextZone)) {
                                 double newSpeed = std::min(currentSpeed + step, maxSpeed);
                                 ((treadmill *)bluetoothManager->device())
                                     ->changeSpeedAndInclination(
