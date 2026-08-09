4e815f225e5bccebd551ae06b0ff72d9fe3126b0
Roberto Viola <cagnulein@gmail.com>
2026-08-09 22:33:56 +0200

    Limit TC rower subscriptions to FTMS

--- src/devices/ftmsrower/ftmsrower.h
+++ src/devices/ftmsrower/ftmsrower.h
@@ -74,6 +74,7 @@ class ftmsrower : public rower {
     bool ICONSOLE_PLUS = false;
     bool FITSHOW = false;
     bool DOMYOS = false;
+    bool TC_ROWER = false;
     bool WDK_PACE_POWER = false;
 
     bool WATER_ROWER = false;
