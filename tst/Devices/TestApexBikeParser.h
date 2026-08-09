ea0ffc3267101b04aab0e1dee1f268f9aef770ad
Roberto Viola <Cagnulein@gmail.com>
2026-08-09 16:12:51 +0200

    test: align Apex WLT8266BM expectations (#4902)
    Co-authored-by: root <root@hermes.proxmox>

--- tst/Devices/TestApexBikeParser.h
+++ tst/Devices/TestApexBikeParser.h
@@ -14,13 +14,13 @@ TEST(ApexBikeWlt8266BmRegressionTest, DistanceCounterMetricsAreDisabledForWlt826
     EXPECT_FALSE(apexbike::usesWlt8266bmDistanceCounterMetrics(QStringLiteral("APEX Bike")));
 }
 
-TEST(ApexBikeWlt8266BmRegressionTest, MatchingDevicesUseZeroX30PacketsForDistanceCounterMetrics) {
+TEST(ApexBikeWlt8266BmRegressionTest, Wlt8266BmPacketsDoNotUseZeroX30DistanceCounterMetrics) {
     const QByteArray movementPacket = QByteArray::fromHex("ea503000020000000082");
     const QByteArray resistancePacket = QByteArray::fromHex("ea503100001a0000009b");
 
-    EXPECT_TRUE(apexbike::isWlt8266bmDistanceCounterMetricsPacket(
+    EXPECT_FALSE(apexbike::isWlt8266bmDistanceCounterMetricsPacket(
         QStringLiteral("WLT8266BM_025B"), movementPacket));
-    EXPECT_TRUE(apexbike::isWlt8266bmDistanceCounterMetricsPacket(
+    EXPECT_FALSE(apexbike::isWlt8266bmDistanceCounterMetricsPacket(
         QStringLiteral("WLT8266BM_0000"), movementPacket));
     EXPECT_FALSE(apexbike::isWlt8266bmDistanceCounterMetricsPacket(
         QStringLiteral("WLT8266BM_025B"), resistancePacket));
