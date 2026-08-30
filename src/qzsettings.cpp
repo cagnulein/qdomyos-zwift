c59a3b7b84317fd40d84db3e8b2fea0e5879fff3
root <root@hermes.proxmox>
2026-08-30 17:37:34 +0200

    fix: keep settings count aligned

--- src/qzsettings.cpp
+++ src/qzsettings.cpp
@@ -1287,7 +1287,7 @@ const QString QZSettings::default_shortcut_start_stop = QStringLiteral("");
 const QString QZSettings::shortcut_stop = QStringLiteral("shortcut_stop");
 const QString QZSettings::default_shortcut_stop = QStringLiteral("");
 
-const uint32_t allSettingsCount = 1005;
+const uint32_t allSettingsCount = 1006;
 
 QVariant allSettings[allSettingsCount][2] = {
     {QZSettings::cryptoKeySettingsProfiles, QZSettings::default_cryptoKeySettingsProfiles},
