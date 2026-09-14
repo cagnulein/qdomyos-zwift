1987698034978e09b6e6f639a7561ef94f760d76
Roberto Viola <Cagnulein@gmail.com>
2026-09-14 20:11:24 +0200

    Fix F-Bike resistance with virtual FTMS

--- src/devices/ftmsbike/ftmsbike.cpp
+++ src/devices/ftmsbike/ftmsbike.cpp
@@ -263,7 +263,8 @@ void ftmsbike::forcePower(int16_t requestPower) {
 
 bool ftmsbike::isManualResistanceBike() const {
     return SMARTBIKE_3DIGIT || TX_500MB_IRON ||
-           bluetoothDevice.name().toUpper().startsWith(QStringLiteral("FBIKE-HEAVY-PRO"));
+           (bluetoothDevice.name().toUpper().startsWith(QStringLiteral("FBIKE-HEAVY-PRO")) &&
+            cscbike::useCustomResistancePowerTable());
 }
 
 void ftmsbike::enableManualResistancePowerAdjustment(resistance_t resistance) {
@@ -516,10 +517,18 @@ void ftmsbike::update() {
             if (rR != currentResistance().value() || lastGearValue != gears()) {
                 bool ergModeNotSupported = (requestPower > 0 && !ergModeSupported);
                 qDebug() << QStringLiteral("writing resistance ") << requestResistance << ergModeNotSupported << requestPower << resistance_lvl_mode;
+                const bool fbikeManualResistance =
+                    bluetoothDevice.name().toUpper().startsWith(QStringLiteral("FBIKE-HEAVY-PRO")) &&
+                    cscbike::useCustomResistancePowerTable();
+                if (fbikeManualResistance) {
+                    if (requestResistance != -1) {
+                        enableManualResistancePowerAdjustment(rR);
+                    }
+                }
                 // if the FTMS is connected, the ftmsCharacteristicChanged event will do all the stuff because it's a
                 // FTMS bike. This condition handles the peloton requests
-                if (((virtualBike && !virtualBike->ftmsDeviceConnected()) || !virtualBike || resistance_lvl_mode || ergModeNotSupported) &&
-                    (requestPower == 0 || requestPower == -1 || resistance_lvl_mode || ergModeNotSupported)) {
+                else if (((virtualBike && !virtualBike->ftmsDeviceConnected()) || !virtualBike || resistance_lvl_mode || ergModeNotSupported) &&
+                         (requestPower == 0 || requestPower == -1 || resistance_lvl_mode || ergModeNotSupported)) {
                     if (DOMYOS) {
                         QDateTime now = QDateTime::currentDateTime();
                         const qint64 sinceLastDomyosResistance = lastDomyosResistanceCommand.msecsTo(now);
@@ -2023,7 +2032,7 @@ void ftmsbike::characteristicWritten(const QLowEnergyCharacteristic &characteris
 }
 
 void ftmsbike::characteristicRead(const QLowEnergyCharacteristic &characteristic, const QByteArray &newValue) {
-    qDebug() << QStringLiteral("characteristicRead ") << characteristic.uuid() << newValue.toHex(' ');
+    qDebug() << QStringLiteral("characteristicRead ") << characteristic.uuid() << newValue.toHex(' '));
 }
 
 void ftmsbike::serviceScanDone(void) {
