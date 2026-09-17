8060200e6fa0143c650d4860cb1885df3bc96476
Roberto Viola <cagnulein@gmail.com>
2026-09-17 10:36:34 +0200

    feat: add NordicTrack GX LE profile

--- src/devices/proformbike/proformbike.h
+++ src/devices/proformbike/proformbike.h
@@ -100,6 +100,7 @@ class proformbike : public bike {
     bool proform_csx210 = false;
     bool nordictrack_vr21 = false;
     bool proform_bike_325_csx_PFEX439210INT_0 = false;
+    bool nordictrack_gx_le = false;
 
 #ifdef Q_OS_IOS
     lockscreen *h = 0;
